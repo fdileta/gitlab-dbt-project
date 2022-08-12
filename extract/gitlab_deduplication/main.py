b073a4197a30b125361b96d6ed77b09148809b1f
ved prakash <vprakash@gitlab.com>
2022-08-12 19:04:27 +0200

    updatedd commit

--- extract/gitlab_deduplication/main.py
+++ extract/gitlab_deduplication/main.py
@@ -1,9 +1,20 @@
 import logging
+import yaml
 from fire import Fire
+from typing import Dict
 
+def manifest_reader(file_path: str) -> Dict[str, Dict]:
+    """
+    Read a yaml manifest file into a dictionary and return it.
+    """
 
-def main (file_path: str='extract/gitlab_deduplication/manifest_deduplication/t_gitlab_com_scd_advance_metadata_manifest.yml')->None:
+    with open(file_path, "r") as file:
+        manifest_dict = yaml.load(file, Loader=yaml.FullLoader)
 
+    return manifest_dict
+
+def main(file_path: str = 'extract/gitlab_deduplication/manifest_deduplication'
+                          '/t_gitlab_com_scd_advance_metadata_manifest.yml') -> None:
     """
     Read table name from manifest file and decide if the table exist in the database. Check if the advance metadata column `_task_instance`
     is present in the table.
@@ -13,8 +24,10 @@ def main (file_path: str='extract/gitlab_deduplication/manifest_deduplication/t_
     Swap the table with original table.
     Drop the swapped temp table.
     """
-    
-
+    # Process the manifest
+    logging.info(f"Reading manifest at location: {file_path}")
+    manifest_dict = manifest_reader(file_path)
+    print(manifest_dict)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
