03c5444491134599fa516bf41efe5778c7dbfd0e
ved prakash <vprakash@gitlab.com>
2022-08-16 14:18:24 +0200

    Updated Dag

--- dags/extract/gitlab_data_deduplication.py
+++ dags/extract/gitlab_data_deduplication.py
@@ -1,11 +1,12 @@
 import os
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow_utils import (
     DATA_IMAGE,
     clone_repo_cmd,
     gitlab_defaults,
-    gitlab_pod_env_vars,
     slack_failed_task,)
 
 from kubernetes_helpers import get_affinity, get_toleration
@@ -19,45 +20,47 @@ from kube_secrets import (
 
 env = os.environ.copy()
 GIT_BRANCH = env["GIT_BRANCH"]
-standard_secrets = [
-    SNOWFLAKE_LOAD_USER,
-    SNOWFLAKE_LOAD_PASSWORD,
-    SNOWFLAKE_ACCOUNT,
-    SNOWFLAKE_LOAD_WAREHOUSE,
-    SNOWFLAKE_LOAD_ROLE,
-]
+pod_env_vars = {
+    "SNOWFLAKE_LOAD_DATABASE": "RAW"
+    if GIT_BRANCH == "master"
+    else f"{GIT_BRANCH.upper()}_RAW",
+    "CI_PROJECT_DIR": "/analytics",
+}
 
-scd_cmd=f"""
+scd_cmd = f"""
             {clone_repo_cmd} &&
             cd analytics/extract/gitlab_deduplication/ &&
             python main.py deduplication ../extract/gitlab_deduplication/manifest_deduplication/t_gitlab_com_deduplication_table_manifest.yaml 
 """
-scd_deduplication = KubernetesPodOperator(
-    **gitlab_defaults,
-    image=DATA_IMAGE,
-    task_id="scd_table_deduplication_task",
-    name="scd_table_deduplication_task",
-    pool="default",
-    secrets=standard_secrets,
-    env_vars=gitlab_pod_env_vars,
-    arguments=[scd_cmd],
-    affinity=get_affinity(True),
-    tolerations=get_toleration(True),
-)
-
-scd_deduplication
-
-scd_deduplication_dag = DAG(
-        "scd_deduplication",
-        scd_deduplication,
-        default_args={
+default_args={
         "catchup": False,
         "depends_on_past": False,
         "on_failure_callback": slack_failed_task,
         "owner": "airflow",
         "retries": 0,
-    },
-        schedule_interval='* * * * *',
-        concurrency=1,
+        "start_date": datetime(2020, 1, 1),
+    }
+
+scd_deduplication_dag = DAG(
+        "scd_deduplication",
+        default_args=default_args,
+        schedule_interval=None
     )
+
+scd_deduplication = KubernetesPodOperator(
+    **gitlab_defaults,
+    image=DATA_IMAGE,
+    task_id="scd_table_deduplication_task",
+    name="scd_table_deduplication_task",
+    secrets=[SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_LOAD_PASSWORD,
+    SNOWFLAKE_LOAD_ROLE,
+    SNOWFLAKE_LOAD_USER,
+    SNOWFLAKE_LOAD_WAREHOUSE,],
+    env_vars=pod_env_vars,
+    arguments=[scd_cmd],
+    affinity=get_affinity(False),
+    tolerations=get_toleration(False),
+    dag=scd_deduplication_dag
+)
 
