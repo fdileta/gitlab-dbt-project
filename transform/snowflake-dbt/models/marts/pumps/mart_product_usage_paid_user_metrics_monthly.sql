8e2137221f45a77f6e1347f34998f47a592e09fb
snalamaru <snalamaru@gitlab.com>
2022-08-18 15:15:33 -0500

    Updated marts and macros

--- transform/snowflake-dbt/models/marts/pumps/mart_product_usage_paid_user_metrics_monthly.sql
+++ transform/snowflake-dbt/models/marts/pumps/mart_product_usage_paid_user_metrics_monthly.sql
@@ -7,7 +7,7 @@
 
 {{ simple_cte([
     ('monthly_saas_metrics','fct_saas_product_usage_metrics_monthly'),
-    ('monthly_sm_metrics','fct_product_usage_wave_1_3_metrics_monthly'),
+    ('monthly_sm_metrics','fct_ping_instance_metric_wave_monthly'),
     ('billing_accounts','dim_billing_account'),
     ('location_country', 'dim_location_country'),
     ('subscriptions', 'dim_subscription'),
@@ -72,14 +72,14 @@
       monthly_sm_metrics.dim_subscription_id,
       NULL                                                                         AS dim_namespace_id,
       NULL                                                                         AS namespace_name,
-      monthly_sm_metrics.uuid,
+      monthly_sm_metrics.dim_instance_id                                           AS uuid,
       monthly_sm_metrics.hostname,
-      {{ get_keyed_nulls('billing_accounts.dim_billing_account_id') }}              AS dim_billing_account_id,
-      {{ get_keyed_nulls('billing_accounts.dim_crm_account_id') }}                      AS dim_crm_account_id,
+      {{ get_keyed_nulls('billing_accounts.dim_billing_account_id') }}             AS dim_billing_account_id,
+      {{ get_keyed_nulls('billing_accounts.dim_crm_account_id') }}                 AS dim_crm_account_id,
       monthly_sm_metrics.dim_subscription_id_original,
       subscriptions.subscription_name,
       subscriptions.subscription_status,
-      most_recent_subscription_version.subscription_status AS subscription_status_most_recent_version,
+      most_recent_subscription_version.subscription_status                         AS subscription_status_most_recent_version,
       subscriptions.term_start_date,
       subscriptions.term_end_date,
       most_recent_subscription_version.subscription_start_date,
@@ -92,24 +92,24 @@
       location_country.country_name,
       location_country.iso_2_country_code,
       location_country.iso_3_country_code,
-      'Self-Managed'                                                                AS delivery_type,
+      'Self-Managed'                                                               AS delivery_type,
       -- Wave 1
       DIV0(
         monthly_sm_metrics.billable_user_count, 
         COALESCE(
           zuora_licenses_per_subscription.license_user_count, 
           monthly_sm_metrics.license_user_count)
-      )                                                                             AS license_utilization,
+      )                                                                            AS license_utilization,
       monthly_sm_metrics.billable_user_count,
       monthly_sm_metrics.active_user_count,
       monthly_sm_metrics.max_historical_user_count,
       COALESCE(
         zuora_licenses_per_subscription.license_user_count, 
-        monthly_sm_metrics.license_user_count)                                      AS license_user_count,
+        monthly_sm_metrics.license_user_count)                                     AS license_user_count,
       IFF(
         zuora_licenses_per_subscription.license_user_count IS NOT NULL, 
         'Zuora',
-        'Service Ping')                                                             AS license_user_count_source,
+        'Service Ping')                                                            AS license_user_count_source,
       -- Wave 2 & 3
       monthly_sm_metrics.umau_28_days_user,
       monthly_sm_metrics.action_monthly_active_users_project_repo_28_days_user,
