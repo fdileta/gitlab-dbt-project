f9705c1ea03f9e5ebc7980467a44eb0f33e9700e
Jean Peguero <jpeguero@gitlab.com>
2022-08-23 13:37:14 +0200

    modify events_400 to incremental model

--- transform/snowflake-dbt/models/legacy/snowplow/combined/400_day/snowplow_structured_events_400.sql
+++ transform/snowflake-dbt/models/legacy/snowplow/combined/400_day/snowplow_structured_events_400.sql
@@ -1,8 +1,28 @@
 {{config({
-    "materialized":"table"
+    "materialized":"incremental",
+    "unique_key":"event_id",
+    "post-hook": ["DELETE FROM {{ this }} WHERE derived_tstamp::DATE < DATEADD('DAY', -400, CURRENT_DATE())"]
   })
 }}
 
 -- depends_on: {{ ref('snowplow_structured_events') }}
 
-{{ schema_union_limit('snowplow_', 'snowplow_structured_events', 'derived_tstamp', 400, database_name=env_var('SNOWFLAKE_PREP_DATABASE')) }}
+{% if is_incremental() %}
+
+WITH filtered_table AS (
+
+  SELECT *
+  FROM {{ this }}
+  WHERE DATE_TRUNC(MONTH, derived_tstamp::DATE) >= DATEADD(MONTH, -10, DATE_TRUNC(MONTH,CURRENT_DATE)) 
+
+)
+
+{% endif %}
+
+SELECT *
+FROM {{ ref('snowplow_structured_events_all') }}
+WHERE DATE_TRUNC(MONTH, derived_tstamp::DATE) >= DATEADD(MONTH, -400, DATE_TRUNC(MONTH,CURRENT_DATE))
+
+{% if is_incremental() %}
+  AND event_id NOT IN (SELECT event_id FROM filtered_table)
+{% endif %}
