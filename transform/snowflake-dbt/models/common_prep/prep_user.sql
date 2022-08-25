0fe69b46e1acd6ba6f5ea4fa277a9f5acba00039
tpoole1 <tpoole@gmail.com>
2022-08-25 14:42:40 -0400

    Initial commit

--- transform/snowflake-dbt/models/common_prep/prep_user.sql
+++ transform/snowflake-dbt/models/common_prep/prep_user.sql
@@ -38,6 +38,64 @@ closest_provider AS (
 
 ), 
 
+user_pref AS (
+    SELECT 
+        user_id                                                       AS user_id,
+        COALESCE(setup_for_company::VARCHAR,'Unknown')                AS setup_for_company
+    FROM
+        prep.gitlab_dotcom.gitlab_dotcom_user_preferences_source
+),
+
+users AS (
+    SELECT 
+        user_id, 
+        COALESCE(role,'Unknown')                                      AS role,
+        COALESCE(TO_DATE(last_activity_on)::VARCHAR,'Unknown')        AS last_activity_date,
+        COALESCE(TO_DATE(last_sign_in_at)::VARCHAR,'Unknown')         AS last_sign_in_date        
+    FROM 
+        prep.sensitive.gitlab_dotcom_users
+),
+
+user_details AS (
+    SELECT 
+        user_id                                                       AS user_id,
+        CASE COALESCE(registration_objective,-1)
+            WHEN 0 THEN 'basics' 
+            WHEN 1 THEN 'move_repository' 
+            WHEN 2 THEN 'code_storage' 
+            WHEN 3 THEN 'exploring' 
+            WHEN 4 THEN 'ci' 
+            WHEN 5 THEN 'other' 
+            WHEN 6 THEN 'joining_team'
+            WHEN -1 THEN 'Unknown'
+        END                                                           AS jobs_to_be_done
+    FROM 
+        prep.gitlab_dotcom.gitlab_dotcom_user_details_source
+),
+
+customer_leads AS (
+    SELECT 
+        user_id                                                       AS user_id,
+        COALESCE(MAX(is_for_business_use)::VARCHAR,'Unknown')         AS for_business_use,
+        COALESCE(MAX(employees_bucket)::VARCHAR,'Unknown')            AS employees_count,
+        COALESCE(MAX(country)::VARCHAR,'Unknown')                     AS country,
+        COALESCE(MAX(state)::VARCHAR,'Unknown')                       AS state
+    FROM prep.customers.customers_db_leads_source
+    GROUP BY
+        user_id
+),
+
+customer_trials AS (
+    SELECT 
+        gitlab_user_id                                                AS gitlab_user_id,
+        COALESCE(MIN(trial_start_date)::VARCHAR,'Unknown')            AS first_trial_start_date,
+        COALESCE(COUNT(distinct order_id)::VARCHAR,'Unknown')         AS trials_count
+    FROM 
+        prod.legacy.customers_db_trials                
+    GROUP BY
+        gitlab_user_id
+),
+
 renamed AS (
 
     SELECT
@@ -72,28 +130,52 @@ renamed AS (
       public_email_domain.classification                               AS public_email_domain_classification,
       source.commit_email_domain                                       AS commit_email_domain,
       commit_email_domain.classification                               AS commit_email_domain_classification,
-      closest_provider.identity_provider                               AS identity_provider 
+      closest_provider.identity_provider                               AS identity_provider,
 
+      -- Expanded Attributes  (Not Found = Row Not found for the Attribute)
+      COALESCE(user_pref.setup_for_company,'Not Found')                 AS setup_for_company,               
+      COALESCE(users.role,'Not Found')                                  AS role,
+      COALESCE(user_details.jobs_to_be_done,'Not Found')                AS jobs_to_be_done,
+      COALESCE(customer_leads.for_business_use,'Not Found')             AS for_business_use,                 
+      COALESCE(customer_leads.employees_count,'Not Found')              AS employees_count,
+      COALESCE(customer_leads.country,'Not Found')                      AS country,
+      COALESCE(customer_leads.state,'Not Found')                        AS state,
+      COALESCE(customer_trials.first_trial_start_date,'Not Found')      AS first_trial_start_date,
+      COALESCE(customer_trials.trials_count,'Not Found')                AS trials_count,
+      COALESCE(users.last_activity_date,'Not Found')                    AS last_activity_date,              
+      COALESCE(users.last_sign_in_date,'Not Found')                     AS last_sign_in_date               
+ 
     FROM source
     LEFT JOIN dim_date
       ON TO_DATE(source.created_at) = dim_date.date_day
-    LEFT JOIN email_classification_dedup AS notification_email_domain
+    LEFT JOIN email_classification_dedup                                AS notification_email_domain
       ON notification_email_domain.domain = source.notification_email_domain
-    LEFT JOIN email_classification_dedup AS email_domain
+    LEFT JOIN email_classification_dedup                                AS email_domain
       ON email_domain.domain = source.email_domain
-    LEFT JOIN email_classification_dedup AS public_email_domain
+    LEFT JOIN email_classification_dedup                                AS public_email_domain
       ON public_email_domain.domain = source.public_email_domain
-    LEFT JOIN email_classification_dedup AS commit_email_domain
+    LEFT JOIN email_classification_dedup                                AS commit_email_domain
       ON commit_email_domain.domain = source.commit_email_domain
-    LEFT JOIN closest_provider AS closest_provider
+    LEFT JOIN closest_provider                                          AS closest_provider
       ON source.user_id = closest_provider.user_id  
-    
+
+    LEFT JOIN user_pref                                                 AS user_pref
+      ON source.user_id = user_pref.user_id
+    LEFT JOIN users                                                     AS users
+      ON source.user_id = users.user_id
+    LEFT JOIN user_details                                              AS user_details
+      ON source.user_id = user_details.user_id
+    LEFT JOIN customer_leads                                            AS customer_leads
+      ON source.user_id = customer_leads.user_id
+    LEFT JOIN customer_trials                                           AS customer_trials
+      ON source.user_id = customer_trials.gitlab_user_id
+
 )
 
 {{ dbt_audit(
     cte_ref="renamed",
     created_by="@mpeychet",
-    updated_by="@iweeks",
+    updated_by="@tpoole",
     created_date="2021-05-31",
-    updated_date="2022-06-20"
+    updated_date="2022-08-25"
 ) }}
