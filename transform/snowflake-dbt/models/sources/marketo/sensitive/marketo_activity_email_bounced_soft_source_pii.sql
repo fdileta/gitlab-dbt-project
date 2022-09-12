710b01b33191aa9469c3501418c3b7b510dbe565
Paul Armstrong <parmstrong@gitlab.com>
2022-09-12 13:04:34 +0200

    Column corrections.

--- transform/snowflake-dbt/models/sources/marketo/sensitive/marketo_activity_email_bounced_soft_source_pii.sql
+++ transform/snowflake-dbt/models/sources/marketo/sensitive/marketo_activity_email_bounced_soft_source_pii.sql
@@ -1,6 +1,6 @@
 WITH source AS (
 
-    SELECT {{ nohash_sensitive_columns('marketo_activity_email_bounced_soft_source', 'contact_id') }}
+    SELECT {{ nohash_sensitive_columns('marketo_activity_email_bounced_soft_source', 'marketo_activity_email_bounced_soft_id') }}
     FROM {{ ref('marketo_activity_email_bounced_soft_source') }}
 
 )
--- transform/snowflake-dbt/models/sources/marketo/sensitive/marketo_activity_email_bounced_source_pii.sql
+++ transform/snowflake-dbt/models/sources/marketo/sensitive/marketo_activity_email_bounced_source_pii.sql
@@ -1,6 +1,6 @@
 WITH source AS (
 
-    SELECT {{ nohash_sensitive_columns('marketo_activity_email_bounced_source', 'contact_id') }}
+    SELECT {{ nohash_sensitive_columns('marketo_activity_email_bounced_source', 'marketo_activity_email_bounced_id') }}
     FROM {{ ref('marketo_activity_email_bounced_source') }}
 
 )
