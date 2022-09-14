527d6ca94f436d305f24beaac1cefe212656e941
Radovan Bacovic <rbacovic@gitlab.com>
2022-09-14 09:07:19 +0200

    Added audit macro.

--- transform/snowflake-dbt/models/workspaces/workspace_product/wk_customers_db_license_versions.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/wk_customers_db_license_versions.sql
@@ -8,3 +8,11 @@ WITH source AS (
 
 SELECT *
 FROM source
+
+{{ dbt_audit(
+    cte_ref="source",
+    created_by="@rbacovic",
+    updated_by="@rbacovic",
+    created_date="2022-09-14",
+    updated_date="2022-09-14"
+) }}
