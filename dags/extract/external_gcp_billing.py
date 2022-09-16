47c3f59584dc7866651fcfc941311ce352b601af
jjstark <jstark@gitlab.com>
2022-09-16 13:35:36 -0700

    update naming

--- dags/extract/external_gcp_billing.py
+++ dags/extract/external_gcp_billing.py
@@ -84,7 +84,7 @@ for export in stream['exports']:
       ],
       env_vars={
           **pod_env_vars,
-					**export,
+          **export,
           "EXPORT_DATE": "{{ execution_date }}",
       },
       affinity=get_affinity(False),
