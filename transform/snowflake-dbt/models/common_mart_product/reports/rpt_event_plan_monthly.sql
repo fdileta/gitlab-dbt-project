bd9a4aad293b2dbc60c9717e309f70d4d0daef64
tpoole1 <tpoole@gmail.com>
2022-09-19 16:46:45 -0400

    change after review
    change after commit

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_event_plan_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_event_plan_monthly.sql
@@ -24,6 +24,8 @@ mart_with_date_range AS (
     mart_event_valid.is_gmau,
     mart_event_valid.is_umau,
     mart_event_valid.dim_user_id,
+    mart_event_valid.plan_id_at_event_date,
+    mart_event_valid.event_created_at,
     dim_date.last_day_of_month AS last_day_of_month,
     dim_date.last_day_of_quarter AS last_day_of_quarter,
     dim_date.last_day_of_fiscal_year AS last_day_of_fiscal_year
@@ -34,52 +36,26 @@ mart_with_date_range AS (
 
 ),
 
-distinct_plan AS (
-
-  SELECT
-     DISTINCT
-     dim_ultimate_parent_namespace_id,
-     event_created_at,
-     plan_id_at_event_date
-  FROM
-    mart_event_valid
-
-),
-
-max_created_at AS (
-
-  SELECT 
+plan_id_by_month AS ( 
+                                                            
+  SELECT                                                    
     dim_ultimate_parent_namespace_id,
     event_calendar_month,
-    MAX(event_created_at) max_event_created_at
-  FROM mart_event_valid
-  GROUP BY
-    dim_ultimate_parent_namespace_id,
-    event_calendar_month
-
-),
-
-month_plan AS (
-
-  SELECT 
-    max_created_at.dim_ultimate_parent_namespace_id,
-    max_created_at.event_calendar_month,
-    distinct_plan.plan_id_at_event_date
-  FROM max_created_at 
-  LEFT JOIN distinct_plan
-    ON max_created_at.dim_ultimate_parent_namespace_id IS NOT DISTINCT FROM distinct_plan.dim_ultimate_parent_namespace_id       -- Treats Nulls as values to Join
-    AND max_created_at.max_event_created_at = distinct_plan.event_created_at
-
-),
+    plan_id_at_event_date
+  FROM mart_with_date_range
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY dim_ultimate_parent_namespace_id, event_calendar_month
+      ORDER BY event_created_at DESC) = 1
+      
+),      
 
 mart_usage_event_plan_monthly AS (
 
   SELECT
-    {{ dbt_utils.surrogate_key(['mart_with_date_range.event_calendar_month', 'month_plan.plan_id_at_event_date', 'mart_with_date_range.event_name']) }} AS event_plan_monthly_id,
+    {{ dbt_utils.surrogate_key(['mart_with_date_range.event_calendar_month', 'plan_id_by_month.plan_id_at_event_date', 'mart_with_date_range.event_name']) }} AS event_plan_monthly_id,
     mart_with_date_range.event_calendar_month,
     mart_with_date_range.event_calendar_quarter,
     mart_with_date_range.event_calendar_year,
-    month_plan.plan_id_at_event_date latest_plan_id,
+    plan_id_by_month.plan_id_at_event_date  plan_id_at_event_month,
     mart_with_date_range.event_name,
     mart_with_date_range.stage_name,
     mart_with_date_range.section_name,
@@ -88,15 +64,15 @@ mart_usage_event_plan_monthly AS (
     mart_with_date_range.is_gmau,
     mart_with_date_range.is_umau,
     COUNT(*) AS event_count,
-    COUNT(DISTINCT(month_plan.dim_ultimate_parent_namespace_id)) AS ultimate_parent_namespace_count,
+    COUNT(DISTINCT(plan_id_by_month.dim_ultimate_parent_namespace_id)) AS ultimate_parent_namespace_count,
     COUNT(DISTINCT( mart_with_date_range.dim_user_id)) AS user_count
   FROM mart_with_date_range
-  LEFT JOIN month_plan
-    ON mart_with_date_range.dim_ultimate_parent_namespace_id IS NOT DISTINCT FROM month_plan.dim_ultimate_parent_namespace_id           -- Treats Nulls as values to Join
-    AND mart_with_date_range.event_calendar_month = month_plan.event_calendar_month
+  LEFT JOIN plan_id_by_month
+    ON COALESCE(mart_with_date_range.dim_ultimate_parent_namespace_id,-5) = COALESCE(plan_id_by_month.dim_ultimate_parent_namespace_id,-5)     -- -1 is already in the table
+    AND mart_with_date_range.event_calendar_month = plan_id_by_month.event_calendar_month
   WHERE mart_with_date_range.event_calendar_month < DATE_TRUNC('month', CURRENT_DATE)
   {{ dbt_utils.group_by(n=12) }}
-  ORDER BY mart_with_date_range.event_calendar_month DESC, month_plan.plan_id_at_event_date DESC
+  ORDER BY mart_with_date_range.event_calendar_month DESC, plan_id_by_month.plan_id_at_event_date DESC
 
 )
 
@@ -105,5 +81,5 @@ mart_usage_event_plan_monthly AS (
     created_by="@dihle",
     updated_by="@tpoole",
     created_date="2022-02-22",
-    updated_date="2022-09-15"
+    updated_date="2022-09-19"
 ) }}
