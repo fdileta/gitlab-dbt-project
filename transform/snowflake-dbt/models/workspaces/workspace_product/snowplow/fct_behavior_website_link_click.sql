e621d42b1c870b4d8e18dbce6624ffa50fb9e91c
Chris Sharp <csharp@gitlab.com>
2022-09-22 17:32:43 +0100

    Initial commit for fct_behavior_website_link_click

---
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_website_link_click.sql
@@ -0,0 +1,51 @@
+{{ config(
+        materialized = "incremental",
+        unique_key = "fct_behavior_website_link_click_sk"
+) }}
+
+{{ simple_cte([
+    ('events', 'prep_snowplow_unstructured_events_all'),
+    ('dim_event', 'dim_behavior_website_event'),
+    ('dim_page', 'dim_behavior_website_page')
+    ])
+}}
+
+, link_click AS (
+
+    SELECT
+      derived_tstamp                                    AS behavior_at,
+      event_name,
+      gsc_environment                                   AS environment,
+      {{ clean_url('page_url_path') }}                  AS clean_url_path,      
+      session_id,
+      lc_targeturl                                      AS link_click_target_url
+    FROM events
+    WHERE event_name = 'link_click'
+
+    {% if is_incremental() %}
+
+    AND derived_tstamp > (SELECT max(behavior_at) FROM {{ this }})
+
+    {% endif %}
+)
+
+, link_click_with_dims AS (
+
+    SELECT
+      dim_event.dim_behavior_website_event_sk,
+      dim_page.dim_behavior_website_page_sk,
+      behavior_at,
+      session_id,
+      link_click_target_url
+    FROM link_click
+    JOIN dim_event ON link_click.event_name = dim_event.event_name
+    JOIN dim_page ON link_click.clean_url_path = dim_page.clean_url_path 
+)
+
+{{ dbt_audit(
+    cte_ref="link_click_with_dims",
+    created_by="@chrissharp",
+    updated_by="@chrissharp",
+    created_date="2022-09-22",
+    updated_date="2022-09-22"
+) }}
---
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/prep_snowplow_unstructured_events_all.sql
@@ -0,0 +1,8 @@
+{{config({
+    "materialized":"view"
+  })
+}}
+
+-- depends_on: {{ ref('snowplow_unstructured_events') }}
+
+{{ schema_union_all('snowplow_', 'snowplow_unstructured_events', database_name=env_var('SNOWFLAKE_PREP_DATABASE')) }}
