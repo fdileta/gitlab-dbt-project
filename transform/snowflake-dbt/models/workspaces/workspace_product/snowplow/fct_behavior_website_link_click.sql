38dc3b4a0ab9451ee977da9d85a826558f26ab9d
Chris Sharp <csharp@gitlab.com>
2022-09-27 17:49:36 +0100

    formatting

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_website_link_click.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_website_link_click.sql
@@ -23,11 +23,11 @@
     INNER JOIN dim_event
       ON events.dim_behavior_website_event_sk = dim_event.dim_behavior_website_event_sk
     WHERE dim_event.event_name = 'link_click'
-    AND behavior_at >= DATEADD(MONTH, -25, CURRENT_DATE)
+      AND behavior_at >= DATEADD(MONTH, -25, CURRENT_DATE)
 
     {% if is_incremental() %}
 
-    AND events.behavior_at > (SELECT max(behavior_at) FROM {{ this }})
+      AND events.behavior_at > (SELECT max(behavior_at) FROM {{ this }})
 
     {% endif %}
 )
@@ -37,5 +37,5 @@
     created_by="@chrissharp",
     updated_by="@chrissharp",
     created_date="2022-09-22",
-    updated_date="2022-09-22"
+    updated_date="2022-09-27"
 ) }}
