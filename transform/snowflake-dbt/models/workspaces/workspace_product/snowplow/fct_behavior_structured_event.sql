9d1f4fd9d4c30c9f2b26cfd313fddc7231f67833
Michelle Cooper <mcooper@gitlab.com>
2022-09-28 16:37:51 -0700

    update fct_behavior_structured_event to limit to 2 months

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event.sql
@@ -1,26 +1,74 @@
-{{ config(
-        materialized = "incremental",
-        unique_key = "behavior_structured_event_pk"
-) }}
+{{config({
+    "materialized":"incremental",
+    "unique_key":"behavior_structured_event_pk"
+  })
 
--- depends_on: {{ ref('snowplow_structured_events') }}
-{{ 
-    simple_cte([
-    ('prep_snowplow_structured_event_all_source', 'prep_snowplow_structured_event_all_source'),
-    ('dim_behavior_website_page', 'dim_behavior_website_page')
-    ])
 }}
+WITH prep_snowplow_structured_event_all_source AS (
 
-, structured_events_w_clean_url AS (
+    SELECT *
+    FROM {{ ref('prep_snowplow_structured_event_all_source') }}
+    WHERE derived_tstamp BETWEEN '2022-08-01' AND '2022-09-30'
+
+), structured_events_w_dim AS (
 
     SELECT
-      prep_snowplow_structured_event_all_source.*, 
-      {{ clean_url('page_url_path') }}   AS clean_url_path,
-      REGEXP_SUBSTR(page_url_path, 'namespace(\\d+)', 1, 1, 'e', 1)     AS dim_namespace_id,
-      REGEXP_SUBSTR(page_url_path, 'project(\\d+)', 1, 1, 'e', 1)   AS dim_project_id,
-      derived_tstamp    AS behavior_at
-    FROM prep_snowplow_structured_event_all_source
 
+      -- Primary Key
+      prep_snowplow_structured_event_all_source.event_id   AS behavior_structured_event_pk,
+
+      -- Foreign Keys
+      dim_behavior_website_page.dim_behavior_website_page_sk,
+      dim_behavior_page_url_path.dim_behavior_page_url_path_sk,
+      dim_behavior_browser.dim_behavior_browser_sk,
+      dim_behavior_operating_system.dim_behavior_operating_system_sk,
+      prep_snowplow_structured_event_all_source.gsc_namespace_id AS dim_namespace_id,
+      prep_snowplow_structured_event_all_source.gsc_project_id AS dim_project_id,
+
+      -- Time Attributes
+      prep_snowplow_structured_event_all_source.dvce_created_tstamp,
+      prep_snowplow_structured_event_all_source.derived_tstamp AS behavior_at,
+
+      -- Degenerate Dimensions (Event Attributes)
+      prep_snowplow_structured_event_all_source.event_action,
+      prep_snowplow_structured_event_all_source.event_category,
+      prep_snowplow_structured_event_all_source.event_label,
+      prep_snowplow_structured_event_all_source.event_property,
+      prep_snowplow_structured_event_all_source.event_value,
+      prep_snowplow_structured_event_all_source.v_tracker,
+      prep_snowplow_structured_event_all_source.session_index,
+      prep_snowplow_structured_event_all_source.app_id,
+      prep_snowplow_structured_event_all_source.session_id,
+      prep_snowplow_structured_event_all_source.user_snowplow_domain_id,
+      prep_snowplow_structured_event_all_source.page_url,
+      prep_snowplow_structured_event_all_source.page_url_scheme,
+      prep_snowplow_structured_event_all_source.page_url_host,
+      prep_snowplow_structured_event_all_source.page_url_fragment,
+      prep_snowplow_structured_event_all_source.contexts,
+
+      -- Degenerate Dimensions (Gitlab Standard Context Attributes)
+      prep_snowplow_structured_event_all_source.gsc_google_analytics_client_id,
+      prep_snowplow_structured_event_all_source.gsc_pseudonymized_user_id,
+      prep_snowplow_structured_event_all_source.gsc_environment,
+      prep_snowplow_structured_event_all_source.gsc_extra,
+      prep_snowplow_structured_event_all_source.gsc_plan,
+      prep_snowplow_structured_event_all_source.gsc_source
+
+    FROM prep_snowplow_structured_event_all_source
+    LEFT JOIN {{ ref('dim_behavior_page_url_path') }}
+      ON prep_snowplow_structured_event_all_source.page_url_path = dim_behavior_page_url_path.page_url_path
+    LEFT JOIN {{ ref('dim_behavior_website_page') }} 
+      ON dim_behavior_page_url_path.clean_url_path = dim_behavior_website_page.clean_url_path
+        AND prep_snowplow_structured_event_all_source.page_url_host = dim_behavior_website_page.page_url_host
+        AND prep_snowplow_structured_event_all_source.app_id = dim_behavior_website_page.app_id
+    LEFT JOIN {{ ref('dim_behavior_browser') }}
+      ON prep_snowplow_structured_event_all_source.browser_name = dim_behavior_browser.browser_name
+        AND prep_snowplow_structured_event_all_source.browser_major_version = dim_behavior_browser.browser_major_version
+        AND prep_snowplow_structured_event_all_source.browser_minor_version = dim_behavior_browser.browser_minor_version
+        AND prep_snowplow_structured_event_all_source.browser_language = dim_behavior_browser.browser_language
+    LEFT JOIN {{ ref('dim_behavior_operating_system') }}
+      ON prep_snowplow_structured_event_all_source.os_name = dim_behavior_operating_system.os_name
+        AND prep_snowplow_structured_event_all_source.os_timezone = dim_behavior_operating_system.os_timezone
     {% if is_incremental() %}
 
     WHERE behavior_at > (SELECT MAX(behavior_at) FROM {{this}})
@@ -29,79 +77,10 @@
 
 )
 
-, structured_events_w_dim AS (
-
-    SELECT
-
-      -- Primary Key
-      {{ dbt_utils.surrogate_key(['structured_events_w_clean_url.event_id']) }}   AS behavior_structured_event_pk,
-
-      -- Foreign Keys
-      dim_behavior_website_page.dim_behavior_website_page_sk,
-      structured_events_w_clean_url.dim_namespace_id,
-      structured_events_w_clean_url.dim_project_id,
-
-      -- Natural Keys
-      structured_events_w_clean_url.event_id,
-      structured_events_w_clean_url.app_id,
-      structured_events_w_clean_url.session_id,
-      structured_events_w_clean_url.user_snowplow_domain_id,
-
-      -- Time Attributes
-      structured_events_w_clean_url.dvce_created_tstamp,
-      structured_events_w_clean_url.behavior_at,
-
-      -- Event Attributes
-      structured_events_w_clean_url.event_action,
-      structured_events_w_clean_url.event_category,
-      structured_events_w_clean_url.event_label,
-      structured_events_w_clean_url.event_property,
-      structured_events_w_clean_url.event_value,
-      structured_events_w_clean_url.v_tracker,
-      structured_events_w_clean_url.session_index,
-      structured_events_w_clean_url.contexts,
-
-      structured_events_w_clean_url.page_url,
-      structured_events_w_clean_url.clean_url_path,
-      structured_events_w_clean_url.page_url_scheme,
-      structured_events_w_clean_url.page_url_host,
-      structured_events_w_clean_url.page_url_path,
-      structured_events_w_clean_url.page_url_fragment,
-
-      structured_events_w_clean_url.browser_name,
-      structured_events_w_clean_url.browser_major_version,
-      structured_events_w_clean_url.browser_minor_version,
-      structured_events_w_clean_url.browser_language,
-      structured_events_w_clean_url.os,
-      structured_events_w_clean_url.os_name,
-      structured_events_w_clean_url.os_manufacturer,
-      structured_events_w_clean_url.os_timezone,
-      structured_events_w_clean_url.browser_engine,
-      structured_events_w_clean_url.device_type,
-      structured_events_w_clean_url.device_is_mobile,
-
-      -- Gitlab Standard Context fields
-      structured_events_w_clean_url.gsc_google_analytics_client_id,
-      structured_events_w_clean_url.gsc_pseudonymized_user_id,
-      structured_events_w_clean_url.gsc_environment,
-      structured_events_w_clean_url.gsc_extra,
-      structured_events_w_clean_url.gsc_namespace_id,
-      structured_events_w_clean_url.gsc_plan,
-      structured_events_w_clean_url.gsc_project_id,
-      structured_events_w_clean_url.gsc_source
-
-    FROM structured_events_w_clean_url
-    LEFT JOIN dim_behavior_website_page 
-      ON structured_events_w_clean_url.clean_url_path = dim_behavior_website_page.clean_url_path
-        AND structured_events_w_clean_url.page_url_host = dim_behavior_website_page.page_url_host
-        AND structured_events_w_clean_url.app_id = dim_behavior_website_page.app_id
-
-)
-
 {{ dbt_audit(
     cte_ref="structured_events_w_dim",
     created_by="@michellecooper",
     updated_by="@michellecooper",
     created_date="2022-09-01",
-    updated_date="2022-09-01"
+    updated_date="2022-09-28"
 ) }}
