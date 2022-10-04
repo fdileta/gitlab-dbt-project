da76351478d9bb54b0bb27d22d8feaf7b81375a2
Lis Vinueza <lvinueza@gitlab.com>
2022-10-04 14:00:18 -0500

    Add source file

--- transform/snowflake-dbt/models/workspaces/workspace_marketing/ga360_session_hit_custom_dimension.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_marketing/ga360_session_hit_custom_dimension.sql
@@ -1,7 +1,7 @@
 WITH source AS (
 
-	SELECT *
-	FROM {{ source('google_analytics_360', 'session_hit_custom_dimension') }}
+    SELECT *
+    FROM {{ref('ga360_session_hit_custom_dimension_source')}}
 
 )
 
