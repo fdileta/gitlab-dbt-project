0e48563210afadd85b1f884312d0a1b44b447ede
Justin Wong <juwong@gitlab.com>
2022-10-06 17:40:26 -0400

    add/update tests

--- extract/test/test_transform_postgres_to_snowflake.py
+++ extract/test/test_transform_postgres_to_snowflake.py
@@ -23,6 +23,7 @@ from extract.saas_usage_ping.transform_postgres_to_snowflake import (
     get_renamed_query_tables,
     get_renamed_table_name,
     is_for_renaming,
+    perform_action_on_query_str,
 )
 
 
@@ -84,12 +85,12 @@ def metric_list() -> set:
     static list of metrics for testing complex subqueries
     """
     return {
-        "usage_activity_by_stage_monthly.create.approval_project_rules_with_more_approvers_than_required",
-        "usage_activity_by_stage_monthly.create.approval_project_rules_with_less_approvers_than_required",
-        "usage_activity_by_stage_monthly.create.approval_project_rules_with_exact_required_approvers",
-        "usage_activity_by_stage.create.approval_project_rules_with_more_approvers_than_required",
-        "usage_activity_by_stage.create.approval_project_rules_with_less_approvers_than_required",
-        "usage_activity_by_stage.create.approval_project_rules_with_exact_required_approvers",
+        "approval_project_rules_with_more_approvers_than_required",
+        "approval_project_rules_with_less_approvers_than_required",
+        "approval_project_rules_with_exact_required_approvers",
+        "approval_project_rules_with_more_approvers_than_required",
+        "approval_project_rules_with_less_approvers_than_required",
+        "approval_project_rules_with_exact_required_approvers",
     }
 
 
@@ -156,65 +157,33 @@ def test_transforming_queries():
     }
 
     actuals_dict = {
-        "counts.boards": "SELECT 'counts.boards' AS counter_name,  "
-        "COUNT(boards.id) AS counter_value, "
-        "TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_boards_dedupe_source AS boards",
-        "counts.clusters_applications_cert_managers": "SELECT 'counts.clusters_applications_cert_managers' AS counter_name,  "
-        "COUNT(DISTINCT clusters.user_id) AS counter_value, "
-        "TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_applications_cert_managers_dedupe_source AS clusters_applications_cert_managers "
-        "INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters "
-        "ON clusters.id = clusters_applications_cert_managers.cluster_id "
-        "WHERE clusters_applications_cert_managers.status IN (11, 3, 5)",
-        "counts.clusters_platforms_eks": "SELECT 'counts.clusters_platforms_eks' AS counter_name,  "
-        "COUNT(clusters.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters "
-        "INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_cluster_providers_aws_dedupe_source AS cluster_providers_aws "
-        "ON cluster_providers_aws.cluster_id = clusters.id "
-        "WHERE clusters.provider_type = 2 "
-        "AND (cluster_providers_aws.status IN (3)) "
-        "AND clusters.enabled = TRUE",
-        "counts.clusters_platforms_gke": "SELECT 'counts.clusters_platforms_gke' AS counter_name,  "
-        "COUNT(clusters.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters "
-        "INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_cluster_providers_gcp_dedupe_source AS cluster_providers_gcp "
-        "ON cluster_providers_gcp.cluster_id = clusters.id "
-        "WHERE clusters.provider_type = 1 "
-        "AND (cluster_providers_gcp.status IN (3)) "
-        "AND clusters.enabled = TRUE",
-        "counts.clusters_platforms_user": "SELECT 'counts.clusters_platforms_user' AS counter_name,  "
-        "COUNT(clusters.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters "
-        "WHERE clusters.provider_type = 0 "
-        "AND clusters.enabled = TRUE",
-        "counts.incident_labeled_issues": "SELECT 'counts.incident_labeled_issues' AS counter_name,  "
-        "COUNT(issues.id) AS counter_value, "
-        "TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_issues_dedupe_source AS issues "
-        "INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_label_links_dedupe_source AS label_links "
-        "ON label_links.target_type = 'Issue' "
-        "AND label_links.target_id = issues.id "
-        "INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_labels_dedupe_source AS labels "
-        "ON labels.id = label_links.label_id "
-        "WHERE labels.title = 'incident' "
-        "AND labels.color = '#CC0033' "
-        "AND labels.description = 'Denotes a disruption "
-        "to IT services and the associated issues require immediate attention'",
-    }
+        "counts": {
+          "boards": "SELECT 'boards' AS counter_name,  COUNT(boards.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_boards_dedupe_source AS boards",
+          "clusters_applications_cert_managers": "SELECT 'clusters_applications_cert_managers' AS counter_name,  COUNT(DISTINCT clusters.user_id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_applications_cert_managers_dedupe_source AS clusters_applications_cert_managers INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters ON clusters.id = clusters_applications_cert_managers.cluster_id WHERE clusters_applications_cert_managers.status IN (11, 3, 5)",
+          "clusters_platforms_eks": "SELECT 'clusters_platforms_eks' AS counter_name,  COUNT(clusters.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_cluster_providers_aws_dedupe_source AS cluster_providers_aws ON cluster_providers_aws.cluster_id = clusters.id WHERE clusters.provider_type = 2 AND (cluster_providers_aws.status IN (3)) AND clusters.enabled = TRUE",
+          "clusters_platforms_gke": "SELECT 'clusters_platforms_gke' AS counter_name,  COUNT(clusters.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_cluster_providers_gcp_dedupe_source AS cluster_providers_gcp ON cluster_providers_gcp.cluster_id = clusters.id WHERE clusters.provider_type = 1 AND (cluster_providers_gcp.status IN (3)) AND clusters.enabled = TRUE",
+          "clusters_platforms_user": "SELECT 'clusters_platforms_user' AS counter_name,  COUNT(clusters.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_clusters_dedupe_source AS clusters WHERE clusters.provider_type = 0 AND clusters.enabled = TRUE",
+          "incident_labeled_issues": "SELECT 'incident_labeled_issues' AS counter_name,  COUNT(issues.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_issues_dedupe_source AS issues INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_label_links_dedupe_source AS label_links ON label_links.target_type = 'Issue' AND label_links.target_id = issues.id INNER JOIN prep.gitlab_dotcom.gitlab_dotcom_labels_dedupe_source AS labels ON labels.id = label_links.label_id WHERE labels.title = 'incident' AND labels.color = '#CC0033' AND labels.description = 'Denotes a disruption to IT services and the associated issues require immediate attention'"
+        }
+  }
 
-    final_sql_query_dict = transform(test_cases_dict)
-
-    for sql_metric, sql_query in final_sql_query_dict.items():
-        #  check did we fix the bug with "JOINprep", should be fixed to "JOIN prep."
+    def check_join_prep(sql_metric, sql_query):
+        """
+        check did we fix the bug with "JOINprep", should be fixed to "JOIN prep."
+        """
         final_sql = sql_query.upper()
+        print(f'\nfinal_sql: {final_sql}')
         assert "JOINPREP" not in final_sql
 
         if "JOIN" in final_sql:
             assert "JOIN PREP" in final_sql
 
         # compare translated query with working SQL
-        assert sql_query == actuals_dict[sql_metric]
+        assert sql_query == actuals_dict['counts'][sql_metric]
+
+    final_sql_query_dict = transform(test_cases_dict)
+
+    perform_action_on_query_str(original_dict=final_sql_query_dict, action=check_join_prep, action_arg_type='both')
 
 
 def test_scalar_subquery():
@@ -228,14 +197,16 @@ def test_scalar_subquery():
     }
 
     actuals_dict = {
-        "counts.snippets": "SELECT 'counts.snippets' AS counter_name,  (SELECT COUNT(snippets.id) FROM prep.gitlab_dotcom.gitlab_dotcom_snippets_dedupe_source AS snippets WHERE snippets.type = 'PersonalSnippet') + (SELECT COUNT(snippets.id) FROM prep.gitlab_dotcom.gitlab_dotcom_snippets_dedupe_source AS snippets WHERE snippets.type = 'ProjectSnippet') AS counter_value, TO_DATE(CURRENT_DATE) AS run_day  "
-    }
+        "counts": {
+          "snippets": "SELECT 'snippets' AS counter_name,  (SELECT COUNT(snippets.id) FROM prep.gitlab_dotcom.gitlab_dotcom_snippets_dedupe_source AS snippets WHERE snippets.type = 'PersonalSnippet') + (SELECT COUNT(snippets.id) FROM prep.gitlab_dotcom.gitlab_dotcom_snippets_dedupe_source AS snippets WHERE snippets.type = 'ProjectSnippet') AS counter_value, TO_DATE(CURRENT_DATE) AS run_day  "
+        }
+      }
 
     final_sql_query_dict = transform(test_cases_dict)
 
-    for sql_metric, sql_query in final_sql_query_dict.items():
+    for sql_metric, sql_query in final_sql_query_dict['counts'].items():
         # compare translated query with working SQL
-        assert sql_query == actuals_dict[sql_metric]
+        assert sql_query == actuals_dict['counts'][sql_metric]
 
 
 def test_regular_subquery_transform():
@@ -268,22 +239,18 @@ def test_regular_subquery_transform():
     }
 
     actuals_dict_subquery = {
-        "usage_activity_by_stage_monthly.create.merge_requests_with_overridden_project_rules": "SELECT 'usage_activity_by_stage_monthly.create.merge_requests_with_overridden_project_rules' "
-        "AS counter_name,  "
-        "COUNT(DISTINCT approval_merge_request_rules.merge_request_id) AS counter_value, "
-        "TO_DATE(CURRENT_DATE) AS run_day   "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_approval_merge_request_rules_dedupe_source AS approval_merge_request_rules "
-        "WHERE approval_merge_request_rules.created_at "
-        "BETWEEN '2021-08-14 12:44:36.596707' AND '2021-09-11 12:44:36.596773' "
-        "AND ((EXISTS ( SELECT 1 "
-        "FROM prep.gitlab_dotcom.gitlab_dotcom_approval_merge_request_rule_sources_dedupe_source AS approval_merge_request_rule_sources WHERE approval_merge_request_rule_sources.approval_merge_request_rule_id = approval_merge_request_rules.id AND NOT EXISTS ( SELECT 1 FROM prep.gitlab_dotcom.gitlab_dotcom_approval_project_rules_dedupe_source AS approval_project_rules WHERE approval_project_rules.id = approval_merge_request_rule_sources.approval_project_rule_id AND EXISTS ( SELECT 1 FROM prep.gitlab_dotcom.gitlab_dotcom_projects_dedupe_source AS projects WHERE projects.id = approval_project_rules.project_id AND projects.disable_overriding_approvers_per_merge_request = FALSE)))) OR(approval_merge_request_rules.modified_from_project_rule = TRUE))"
+        "usage_activity_by_stage_monthly": {
+            "create": {
+            "merge_requests_with_overridden_project_rules": "SELECT 'merge_requests_with_overridden_project_rules' AS counter_name,  COUNT(DISTINCT approval_merge_request_rules.merge_request_id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_approval_merge_request_rules_dedupe_source AS approval_merge_request_rules WHERE approval_merge_request_rules.created_at BETWEEN '2021-08-14 12:44:36.596707' AND '2021-09-11 12:44:36.596773' AND ((EXISTS ( SELECT 1 FROM prep.gitlab_dotcom.gitlab_dotcom_approval_merge_request_rule_sources_dedupe_source AS approval_merge_request_rule_sources WHERE approval_merge_request_rule_sources.approval_merge_request_rule_id = approval_merge_request_rules.id AND NOT EXISTS ( SELECT 1 FROM prep.gitlab_dotcom.gitlab_dotcom_approval_project_rules_dedupe_source AS approval_project_rules WHERE approval_project_rules.id = approval_merge_request_rule_sources.approval_project_rule_id AND EXISTS ( SELECT 1 FROM prep.gitlab_dotcom.gitlab_dotcom_projects_dedupe_source AS projects WHERE projects.id = approval_project_rules.project_id AND projects.disable_overriding_approvers_per_merge_request = FALSE)))) OR(approval_merge_request_rules.modified_from_project_rule = TRUE))"
+            }
+        }
     }
 
     final_sql_query_dict = transform(test_cases_dict_subquery)
 
-    for sql_metric, sql_query in final_sql_query_dict.items():
+    for sql_metric, sql_query in final_sql_query_dict['usage_activity_by_stage_monthly']['create'].items():
         # compare translated query with working SQL
-        assert sql_query == actuals_dict_subquery[sql_metric]
+        assert sql_query == actuals_dict_subquery['usage_activity_by_stage_monthly']['create'][sql_metric]
 
 
 def test_optimize_token_size():
@@ -391,19 +358,21 @@ def test_subquery_complex(transformed_dict, list_of_metrics):
 
     final_sql_query_dict = transformed_dict
 
-    for metric_name, metric_sql in final_sql_query_dict.items():
-        assert "approval" in metric_name
-        assert metric_name in list_of_metrics
-        assert metric_name in metric_sql
+    for usage_key, create_dict in final_sql_query_dict.items():
+        for create_key, metrics_dict in create_dict.items():
+            for metric_name, metric_sql in metrics_dict.items():
+                assert "approval" in metric_name
+                assert metric_name in list_of_metrics
+                assert metric_name in metric_sql
 
-        assert (
-            metric_sql.upper().count("SELECT") == expect_value_select
-        )  # expect 2 SELECT statement
-        assert (
-            metric_sql.upper().count("FROM") == expect_value_from
-        )  # expect 2 FROM statements
+                assert (
+                    metric_sql.upper().count("SELECT") == expect_value_select
+                )  # expect 2 SELECT statement
+                assert (
+                    metric_sql.upper().count("FROM") == expect_value_from
+                )  # expect 2 FROM statements
 
-        assert metric_sql.count("(") == metric_sql.count(")")  # query parsed properly
+                assert metric_sql.count("(") == metric_sql.count(")")  # query parsed properly
 
 
 def test_transform_having_clause(transformed_dict, list_of_metrics):
@@ -418,15 +387,72 @@ def test_transform_having_clause(transformed_dict, list_of_metrics):
 
     final_sql_query_dict = transformed_dict
 
-    for metric_name, metric_sql in final_sql_query_dict.items():
-        assert ".id" in metric_sql
-        assert "MAX(" in metric_sql
-        assert "COUNT(approval_project_rules_users.id)" in metric_sql
-        assert "MAX(approvals_required)" in metric_sql
-        assert "subquery" in metric_sql
-        assert metric_sql.count("(") == metric_sql.count(")")
-        assert metric_name in list_of_metrics
-        assert metric_name in metric_sql
+
+    for usage_key, create_dict in final_sql_query_dict.items():
+        for create_key, metrics_dict in create_dict.items():
+            for metric_name, metric_sql in metrics_dict.items():
+                assert ".id" in metric_sql
+                assert "MAX(" in metric_sql
+                assert "COUNT(approval_project_rules_users.id)" in metric_sql
+                assert "MAX(approvals_required)" in metric_sql
+                assert "subquery" in metric_sql
+                assert metric_sql.count("(") == metric_sql.count(")")
+                assert metric_name in list_of_metrics
+                assert metric_name in metric_sql
+
+
+def test_nested_structure():
+    """
+    Test that an arbitrarily nested payload will return
+    a correctly transformed output, including the following:
+        - only select children nodes are kept
+        - original json structure of remaining nodes is preserved
+    """
+
+    test_cases_dict = {
+      "active_user_count": "SELECT COUNT(\"users\".\"id\") FROM \"users\" WHERE (\"users\".\"state\" IN ('active')) AND (\"users\".\"user_type\" IS NULL OR \"users\".\"user_type\" IN (6, 4))",
+      "counts": {
+        "assignee_lists": "SELECT COUNT(\"lists\".\"id\") FROM \"lists\" WHERE \"lists\".\"list_type\" = 3",
+        "ci_builds": "SELECT COUNT(\"ci_builds\".\"id\") FROM \"ci_builds\" WHERE \"ci_builds\".\"type\" = 'Ci::Build'",
+        "ci_triggers": {
+          "arbitrary_key": "SELECT COUNT(\"ci_triggers\".\"id\") FROM \"ci_triggers\""
+        },
+        "ci_internal_pipelines": -1
+      },
+      "counts_weekly": {
+        "aggregated_metrics": {
+          "compliance_features_track_unique_visits_union": 1135,
+          "arbitrary_key": {
+            "arbitrary_key2": {
+              "arbitrary_key3": "SELECT 1"
+            }
+          }
+        }
+      }
+    }
+
+    actuals_dict = {
+      "active_user_count": "SELECT 'active_user_count' AS counter_name,  COUNT(users.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_users_dedupe_source AS users WHERE (users.state IN ('active')) AND (users.user_type IS NULL OR users.user_type IN (6, 4))",
+      "counts": {
+        "assignee_lists": "SELECT 'assignee_lists' AS counter_name,  COUNT(lists.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_lists_dedupe_source AS lists WHERE lists.list_type = 3",
+        "ci_builds": "SELECT 'ci_builds' AS counter_name,  COUNT(ci_builds.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_ci_builds_dedupe_source AS ci_builds WHERE ci_builds.type = 'Ci::Build'",
+        "ci_triggers": {
+          "arbitrary_key": "SELECT 'arbitrary_key' AS counter_name,  COUNT(ci_triggers.id) AS counter_value, TO_DATE(CURRENT_DATE) AS run_day   FROM prep.gitlab_dotcom.gitlab_dotcom_ci_triggers_dedupe_source AS ci_triggers"
+        }
+      },
+      "counts_weekly": {
+        "aggregated_metrics": {
+          "arbitrary_key": {
+            "arbitrary_key2": {
+              "arbitrary_key3": "SELECT 'arbitrary_key3' AS counter_name,  1 AS counter_value, TO_DATE(CURRENT_DATE) AS run_day  "
+            }
+          }
+        }
+      }
+    }
+
+    final_sql_query_dict = transform(test_cases_dict)
+    assert actuals_dict == final_sql_query_dict
 
 
 def test_get_transformed_token_list():
