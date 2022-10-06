9802c5202050d3b8736ea83b62e9c68e246502f1
Paul Armstrong <parmstrong@gitlab.com>
2022-10-06 13:58:23 +0200

    Debugging.

--- orchestration/manage_snowflake.py
+++ orchestration/manage_snowflake.py
@@ -445,8 +445,10 @@ class SnowflakeManager:
 
     def clone_models_v2_testing(self, *model_input):
 
-        for i in model_input:
-            print(i)
+        print(model_input)
+        input_set = set([i for i in model_input])
+        input_list = list(input_set)
+        print(input_list)
 
 if __name__ == "__main__":
     snowflake_manager = SnowflakeManager(env.copy())
