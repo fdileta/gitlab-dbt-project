e676a68702c7c0e7c406bf4abfd5c6c7082da184
Paul Armstrong <parmstrong@gitlab.com>
2022-10-07 15:22:44 +0200

    Test sort by length.

--- orchestration/clone_models_testing.py
+++ orchestration/clone_models_testing.py
@@ -17,10 +17,48 @@ import argparse
 # Set logging defaults
 logging.basicConfig(stream=sys.stdout, level=20)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('first_string', nargs='+')
-args = parser.parse_args()
-
-print(args.first_string)
 
 
+class SnowflakeManager:
+    def __init__(self, config_vars: Dict):
+        self.engine = create_engine(
+            URL(
+                user=config_vars["SNOWFLAKE_USER"],
+                password=config_vars["SNOWFLAKE_PASSWORD"],
+                account=config_vars["SNOWFLAKE_ACCOUNT"],
+                role=config_vars["SNOWFLAKE_SYSADMIN_ROLE"],
+                warehouse=config_vars["SNOWFLAKE_LOAD_WAREHOUSE"],
+            )
+        )
+
+        # Snowflake database name should be in CAPS
+        # see https://gitlab.com/meltano/analytics/issues/491
+        self.branch_name = config_vars["BRANCH_NAME"].upper()
+        self.prep_database = "{}_PREP".format(self.branch_name)
+        self.prod_database = "{}_PROD".format(self.branch_name)
+        self.raw_database = "{}_RAW".format(self.branch_name)
+
+
+
+    def clone_models_v2_testing(self, model_input):
+
+        input_list = model_input.splitlines()
+        output_list = []
+        for i in input_list:
+            d = json.loads(i)
+            actual_dependencies = [n for n in d.get('depends_on').get('nodes') if 'seed' not in n]
+            d["actual_dependencies"] = actual_dependencies
+            output_list.append(d)
+
+        for s in sorted(output_list, key=lambda i: len(i['actual_dependencies'])):
+            print(len(s.get('actual_dependencies')))
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('INPUT', nargs='+')
+    args = parser.parse_args()
+
+    snowflake_manager = SnowflakeManager(env.copy())
+    snowflake_manager.clone_models_v2_testing(args.INPUT)
