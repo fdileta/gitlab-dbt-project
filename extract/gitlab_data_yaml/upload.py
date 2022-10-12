53a847566f0c451eb1aa06e2b49a6c9794e1b2ec
ved prakash <vprakash@gitlab.com>
2022-10-12 12:16:34 +0200

    Pylint and black formatting

--- extract/gitlab_data_yaml/upload.py
+++ extract/gitlab_data_yaml/upload.py
@@ -2,12 +2,13 @@
 import logging
 import subprocess
 import sys
-import requests
 import base64
 import json
+from os import environ as env
+import traceback
+import requests
 import yaml
 
-from os import environ as env
 
 from gitlabdata.orchestration_utils import (
     snowflake_engine_factory,
@@ -36,14 +37,14 @@ if __name__ == "__main__":
     )
 
     pi_internal_hb_file_dict = dict(
-        #corporate_finance_pi="corporate_finance",
         dev_section_pi="dev_section",
         enablement_section_pi="enablement_section",
         ops_section_pi="ops_section",
         product_pi="product",
-        recruiting_pi="recruiting",
         sales_pi="sales",
-        secure_and_protect_section_pi="secure_and_protect_section",
+        # recruiting_pi="recruiting",
+        # secure_and_protect_section_pi="secure_and_protect_section",
+        # corporate_finance_pi="corporate_finance",
     )
 
     comp_calc_dict = dict(
@@ -60,16 +61,14 @@ if __name__ == "__main__":
     pi_url = f"{HANDBOOK_URL}performance_indicators/"
 
     # Internal handbook url
-    pi_internal_hb_url = "https://gitlab.com/api/v4/projects/26282493/repository/files/data%2Fperformance_indicators%2F"
+    PI_INTERNAL_HB_URL = "https://gitlab.com/api/v4/projects/26282493/repository/files/data%2Fperformance_indicators%2F"
 
-    comp_calc_url = (
+    COMP_CALC_URL = (
         "https://gitlab.com/api/v4/projects/21924975/repository/files/data%2F"
     )
 
     TEAM_URL = "https://about.gitlab.com/company/team/"
-    usage_ping_metrics_url = "https://gitlab.com/api/v4/usage_data/metric_definitions/"
-
-    job_failed = False
+    USAGE_PING_METRICS_URL = "https://gitlab.com/api/v4/usage_data/metric_definitions/"
 
     def request_download_decode_upload(
         table_name, file_name, base_url, private_token=None, suffix_url=None
@@ -79,15 +78,15 @@ if __name__ == "__main__":
         it to external stage of snowflake. Once the file gets loaded it will be deleted from external stage.
         This function can be extended but for now this used for the decoding the encoded content"""
         logging.info(f"Downloading {file_name} to {file_name}.json file.")
-
         # Check if there is private token issued for the URL
         if private_token is not None:
             request_url = f"{base_url}{file_name}{suffix_url}"
-            response = requests.request("GET", request_url, headers={"Private-Token": private_token})
-
+            response = requests.request(
+                "GET", request_url, headers={"Private-Token": private_token}, timeout=10
+            )
         # Load the content in json
         api_response_json = response.json()
-        #check if the file is empty or not present.
+        # check if the file is empty or not present.
         record_count = len(api_response_json)
         if record_count > 1:
             # Get the content from response
@@ -95,19 +94,20 @@ if __name__ == "__main__":
             message_bytes = base64.b64decode(file_content)
             output_json_request = yaml.load(message_bytes, Loader=yaml.Loader)
             # write to the Json file
-            with open(f"{file_name}.json", "w") as f:
-                json.dump(output_json_request, f, indent=4)
+            with open(f"{file_name}.json", "w", encoding="UTF-8") as file_name_json:
+                json.dump(output_json_request, file_name_json, indent=4)
             logging.info(f"Uploading to {file_name}.json to Snowflake stage.")
 
             snowflake_stage_load_copy_remove(
-            f"{file_name}.json",
-            "gitlab_data_yaml.gitlab_data_yaml_load",
-            f"gitlab_data_yaml.{table_name}",
-            snowflake_engine,
-        )
+                f"{file_name}.json",
+                "gitlab_data_yaml.gitlab_data_yaml_load",
+                f"gitlab_data_yaml.{table_name}",
+                snowflake_engine,
+            )
         else:
-            logging.error(f"The file for {file_name} is either empty or the location has changed investigate")
-            job_failed = True
+            logging.error(
+                f"The file for {file_name} is either empty or the location has changed investigate"
+            )
 
     def curl_and_upload(table_name, file_name, base_url, private_token=None):
         """This function uses Curl to download the file and convert the YAML to JSON.
@@ -119,9 +119,7 @@ if __name__ == "__main__":
             json_file_name = file_name.split(".yml")[0]
         else:
             json_file_name = file_name
-
         logging.info(f"Downloading {file_name} to {json_file_name}.json file.")
-
         if private_token is not None:
             header = f'--header "PRIVATE-TOKEN: {private_token}"'
             command = f"curl {header} '{base_url}{file_name}%2Eyml/raw?ref=main' | yaml2json -o {json_file_name}.json"
@@ -129,10 +127,13 @@ if __name__ == "__main__":
             command = f"curl {base_url}{file_name} | yaml2json -o {json_file_name}.json"
 
         try:
-            p = subprocess.run(command, shell=True)
-            p.check_returncode()
-        except:
-            job_failed = True
+            process_check = subprocess.run(command, shell=True, check=True)
+            process_check.check_returncode()
+        except:  # pylint: disable=bare-except
+            traceback.print_exc()
+            logging.error(
+                f"The file for {file_name} is either empty or the location has changed investigate"
+            )
 
         logging.info(f"Uploading to {json_file_name}.json to Snowflake stage.")
 
@@ -152,20 +153,16 @@ if __name__ == "__main__":
     # Iterate over Internal handbook
     for key, value in pi_internal_hb_file_dict.items():
         request_download_decode_upload(
-            key, value, pi_internal_hb_url, gitlab_in_hb_token, f"%2Eyml?ref=main"
+            key, value, PI_INTERNAL_HB_URL, gitlab_in_hb_token, "%2Eyml?ref=main"
         )
 
     for key, value in comp_calc_dict.items():
         curl_and_upload(
             key,
             value,
-            comp_calc_url,
+            COMP_CALC_URL,
             config_dict["GITLAB_ANALYTICS_PRIVATE_TOKEN"],
         )
 
     curl_and_upload("team", "team.yml", TEAM_URL)
-    curl_and_upload("usage_ping_metrics", "", usage_ping_metrics_url)
-
-    if job_failed:
-        logging.error(f"Search for value 'ERROR:root:The file for'in the log file for internal handbook extraction.")
-        sys.exit(1)
+    curl_and_upload("usage_ping_metrics", "", USAGE_PING_METRICS_URL)
