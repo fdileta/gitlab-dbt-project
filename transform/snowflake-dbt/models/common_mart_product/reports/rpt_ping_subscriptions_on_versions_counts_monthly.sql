78f4510367d234f9a15751e38ccd92f43c836b55
Carolyn Braza <cbraza@gitlab.com>
2022-10-13 17:02:15 -0700

    fix joins, formatting

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_subscriptions_on_versions_counts_monthly.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_subscriptions_on_versions_counts_monthly.sql
@@ -7,14 +7,13 @@
     ('metric_opt_in', 'rpt_ping_metric_first_last_versions'),
     ('latest_subscriptions', 'rpt_ping_latest_subscriptions_monthly')
     ])
-
-}}
+}},
 
 /*
-Attach metrics_path to subscription IF the subscription is on a version which it can report on
+Attach metrics_path to subscription IF the subscription is on a version with the metric instrumented
 */
 
-, latest_subscriptions_by_metric AS (
+latest_subscriptions_by_metric AS (
 
   SELECT
     latest_subscriptions.ping_created_date_month                        AS ping_created_date_month,
@@ -24,18 +23,20 @@ Attach metrics_path to subscription IF the subscription is on a version which it
     metric_opt_in.ping_edition                                          AS ping_edition,
     metric_opt_in.metrics_path                                          AS metrics_path
   FROM latest_subscriptions
-    INNER JOIN metric_opt_in
-      ON latest_subscriptions.major_minor_version_id
-        BETWEEN metric_opt_in.first_major_minor_version_id_with_counter AND metric_opt_in.last_major_minor_version_id_with_counter
-        AND latest_subscriptions.ping_edition = metric_opt_in.ping_edition
-      {{ dbt_utils.group_by(n=6)}}
+  INNER JOIN metric_opt_in
+    ON latest_subscriptions.major_minor_version_id BETWEEN metric_opt_in.first_major_minor_version_id_with_counter AND metric_opt_in.last_major_minor_version_id_with_counter
+      AND latest_subscriptions.version_is_prerelease = metric_opt_in.version_is_prerelease
+  {{ dbt_utils.group_by(n=6) }}
+
+),
+
 /*
 Aggregate CTE to determine count of arr, subscriptions and licensed users for each month/metric.
 */
 
-), agg_subscriptions AS (
+agg_subscriptions AS (
 
-SELECT
+  SELECT
     {{ dbt_utils.surrogate_key(['ping_created_date_month', 'metrics_path', 'ping_edition']) }}        AS ping_subscriptions_on_versions_counts_monthly_id,
     ping_created_date_month                                                                           AS ping_created_date_month,
     metrics_path                                                                                      AS metrics_path,
@@ -43,15 +44,15 @@ SELECT
     SUM(arr)                                                                                          AS total_arr,
     COUNT(DISTINCT latest_subscription_id)                                                            AS total_subscription_count,
     SUM(licensed_user_count)                                                                          AS total_licensed_users
-FROM latest_subscriptions_by_metric
-    {{ dbt_utils.group_by(n=4)}}
+  FROM latest_subscriptions_by_metric
+  {{ dbt_utils.group_by(n=4) }}
 
 )
 
  {{ dbt_audit(
      cte_ref="agg_subscriptions",
      created_by="@icooper-acp",
-     updated_by="@snalamaru",
+     updated_by="@cbraza",
      created_date="2022-04-20",
-     updated_date="2022-06-08"
+     updated_date="2022-10-14"
  ) }}
