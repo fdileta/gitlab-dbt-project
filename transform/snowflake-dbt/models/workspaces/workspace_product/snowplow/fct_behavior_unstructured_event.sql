ce74a8997125f3ec2ef9677433207c49a9543b37
Chris Sharp <csharp@gitlab.com>
2022-10-14 14:50:47 +0100

    include browser and os into unstructured_event

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_unstructured_event.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_unstructured_event.sql
@@ -7,7 +7,9 @@
 {{ simple_cte([
     ('events', 'prep_snowplow_unstructured_events_all'),
     ('dim_event', 'dim_behavior_event'),
-    ('dim_page', 'dim_behavior_website_page')
+    ('dim_page', 'dim_behavior_website_page'),
+    ('dim_behavior_browser', 'dim_behavior_browser'),
+    ('dim_behavior_operating_system', 'dim_behavior_operating_system')
     ])
 }}
 
@@ -28,7 +30,13 @@
       cf_type                                           AS change_form_type,
       cf_elementid                                      AS change_form_element_id,
       ff_elementid                                      AS focus_form_element_id,
-      ff_nodename                                       AS focus_form_node_name
+      ff_nodename                                       AS focus_form_node_name,
+      browser_name,
+      browser_major_version,
+      browser_minor_version,
+      browser_language,
+      os_name,
+      os_timezone
     FROM events
     WHERE derived_tstamp >= DATEADD(MONTH, -25, CURRENT_DATE)
 
@@ -39,7 +47,7 @@
     {% endif %}
 )
 
-, unstructured_event_with_dims AS (
+, unstruct_event_with_dims AS (
 
     SELECT
 
@@ -53,6 +61,8 @@
       -- Surrogate Keys
       dim_event.dim_behavior_event_sk,
       dim_page.dim_behavior_website_page_sk,
+      dim_behavior_browser.dim_behavior_browser_sk,
+      dim_behavior_operating_system.dim_behavior_operating_system_sk,
 
       --Time Attributes
       behavior_at,
@@ -75,11 +85,19 @@
       ON unstruct_event.clean_url_path = dim_page.clean_url_path 
         AND unstruct_event.page_url_host = dim_page.page_url_host
         AND unstruct_event.app_id = dim_page.app_id
+    LEFT JOIN dim_behavior_browser
+      ON unstruct_event.browser_name = dim_behavior_browser.browser_name
+        AND unstruct_event.browser_major_version = dim_behavior_browser.browser_major_version
+        AND unstruct_event.browser_minor_version = dim_behavior_browser.browser_minor_version
+        AND unstruct_event.browser_language = dim_behavior_browser.browser_language
+    LEFT JOIN dim_behavior_operating_system
+      ON unstruct_event.os_name = dim_behavior_operating_system.os_name
+        AND unstruct_event.os_timezone = dim_behavior_operating_system.os_timezone
       
 )
 
 {{ dbt_audit(
-    cte_ref="unstructured_event_with_dims",
+    cte_ref="unstruct_event_with_dims",
     created_by="@chrissharp",
     updated_by="@chrissharp",
     created_date="2022-09-27",
