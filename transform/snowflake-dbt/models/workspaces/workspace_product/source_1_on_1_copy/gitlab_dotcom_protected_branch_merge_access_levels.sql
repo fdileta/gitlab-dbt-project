4a0ec5b18e75d8ce6b0f19154c1b339577e4724c
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2022-10-17 15:00:46 +0000

    Update gitlab_dotcom_protected_branch_merge_access_levels.sql

--- transform/snowflake-dbt/models/workspaces/workspace_product/source_1_on_1_copy/gitlab_dotcom_protected_branch_merge_access_levels.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/source_1_on_1_copy/gitlab_dotcom_protected_branch_merge_access_levels.sql
@@ -1,7 +1,7 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ ref('gitlab_dotcom_protected_branch_merge_access_levels') }}
+    FROM {{ ref('gitlab_dotcom_protected_branch_merge_access_levels_source') }}
 
 )
 
