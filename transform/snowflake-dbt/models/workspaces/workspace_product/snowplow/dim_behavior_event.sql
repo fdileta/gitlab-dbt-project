18e59776ab0e79b924704ee6a1773de627bbf815
Chris Sharp <csharp@gitlab.com>
2022-10-18 14:14:49 +0100

    add fields to dim_behavior_event

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/dim_behavior_event.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/dim_behavior_event.sql
@@ -15,30 +15,37 @@
       event_name,
       platform,
       gsc_environment       AS environment,
+      se_category           AS event_category,
+      se_action             AS event_action,
+      se_label              AS event_label,
+      se_property           AS event_property,
       MAX(derived_tstamp)   AS max_timestamp
     FROM events
-    WHERE true
 
     {% if is_incremental() %}
     
-    AND derived_tstamp > (SELECT MAX(max_timestamp) FROM {{this}})
+    WHERE derived_tstamp > (SELECT MAX(max_timestamp) FROM {{this}})
     
     {% endif %}
 
-    {{ dbt_utils.group_by(n=4) }}
+    {{ dbt_utils.group_by(n=8) }}
 )
 
 , final AS (
 
     SELECT
       -- Surrogate Key
-      {{ dbt_utils.surrogate_key(['event','event_name','platform','environment']) }} AS dim_behavior_event_sk,
+      {{ dbt_utils.surrogate_key(['event', 'event_name', 'platform', 'environment', 'event_category', 'event_action', 'event_label', 'event_property']) }} AS dim_behavior_event_sk,
 
       -- Natural Keys
       event,
       event_name,
       platform,
       environment,
+      event_category,
+      event_action,
+      event_label,
+      event_property,
 
       --Time Attributes for Incremental Load
       max_timestamp
