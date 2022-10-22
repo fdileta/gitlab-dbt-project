61bcdd8e5dbc95378bb95fd9dc6ab79809db2c60
Jueun Kim <jkim@gitlab.com>
2022-10-22 14:50:42 +0200

    from source layer

--- transform/snowflake-dbt/models/workspaces/workspace_sales/wk_sales_tableau_rls_roles.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/wk_sales_tableau_rls_roles.sql
@@ -3,7 +3,8 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ source('sheetload', 'sales_analytics_tableau_rls_roles') }}
+    --FROM {{ source('sheetload', 'sales_analytics_tableau_rls_roles') }}
+    FROM {{ ref('sheetload_sales_analytics_tableau_rls_roles_source') }}
 
 )
 
--- transform/snowflake-dbt/models/workspaces/workspace_sales/wk_sales_tableau_rls_users.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/wk_sales_tableau_rls_users.sql
@@ -3,7 +3,8 @@
 WITH source AS (
 
     SELECT *
-    FROM {{ source('sheetload', 'sales_analytics_tableau_rls_users') }}
+    --FROM {{ source('sheetload', 'sales_analytics_tableau_rls_users') }}
+    FROM {{ ref('sheetload_sales_analytics_tableau_rls_users_source') }}
 
 )
 
