c4f2b279f9727ee286f4e06c46b53234b57759d4
Paul Armstrong <parmstrong@gitlab.com>
2022-10-28 09:31:41 +0200

    Remove additional quotes.

--- orchestration/clone_dbt_models_select.py
+++ orchestration/clone_dbt_models_select.py
@@ -139,7 +139,7 @@ class DbtModelClone:
 
         find_db_name = f"{find_db_name.split(' ')[0]} \"{find_db_name.split(' ')[1]}\""
 
-        new_first_line = f"{first_line[:dll_input.find('view')]}\"{find_db_name}\"{first_line[dll_input.find('.'):]}"
+        new_first_line = f"{first_line[:dll_input.find('view')]}{find_db_name}{first_line[dll_input.find('.'):]}"
         replaced_file = [
             f.replace("PREP", self.prep_database).replace("PROD", self.prod_database)
             for f in split_file
