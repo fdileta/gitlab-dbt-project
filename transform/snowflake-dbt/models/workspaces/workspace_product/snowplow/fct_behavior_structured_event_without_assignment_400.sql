2f70a354db9a049128d46afc9f6ad7f2b4894b63
Chris Sharp <csharp@gitlab.com>
2022-11-01 17:15:56 +0000

    add 190 and 400 day versions of structured event

---
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event_without_assignment_400.sql
@@ -0,0 +1,36 @@
+{{ config(
+    materialized='incremental',
+    unique_key='behavior_structured_event_pk',
+    on_schema_change='sync_all_columns',
+    post_hook=["{{ rolling_window_delete('behavior_at','day',400) }}"]        
+) }}
+
+WITH source_190 AS (
+
+  SELECT
+    {{ 
+      dbt_utils.star(from=ref('fct_behavior_structured_event_without_assignment'), 
+      except=[
+        'CREATED_BY',
+        'UPDATED_BY',
+        'MODEL_CREATED_DATE',
+        'MODEL_UPDATED_DATE',
+        'DBT_CREATED_AT',
+        'DBT_UPDATED_AT'
+        ]) 
+    }}
+  FROM {{ ref('fct_behavior_structured_event_without_assignment') }}
+  WHERE DATE_TRUNC(MONTH, behavior_at) >= DATEADD(DAY, -400, DATE_TRUNC(DAY, CURRENT_DATE))
+    {% if is_incremental() %}
+      AND behavior_at >= (SELECT MAX(behavior_at) FROM {{ this }})
+    {% endif %}
+
+)
+
+{{ dbt_audit(
+    cte_ref="source_190",
+    created_by="@chrissharp",
+    updated_by="@chrissharp",
+    created_date="2022-11-01",
+    updated_date="2022-11-01"
+) }}
