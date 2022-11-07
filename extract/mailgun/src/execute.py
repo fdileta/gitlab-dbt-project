c3503e0400e5028b7788956cbca651fd14fbf669
Paul Armstrong <parmstrong@gitlab.com>
2022-11-07 16:12:54 +0200

    Add SSL error handling.

--- extract/mailgun/src/execute.py
+++ extract/mailgun/src/execute.py
@@ -2,12 +2,14 @@
 import datetime
 import json
 import sys
+import time
 from email import utils
 from logging import info, basicConfig, getLogger, error
 from os import environ as env
 from typing import Dict, List
 
 import requests
+from requests.exceptions import SSLError
 from fire import Fire
 
 from gitlabdata.orchestration_utils import (
@@ -62,7 +64,12 @@ def extract_logs(event: str, start_date: datetime.datetime) -> List[Dict]:
 
         while True:
             if page_token:
-                response = requests.get(page_token, auth=("api", api_key))
+                try:
+                    response = requests.get(page_token, auth=("api", api_key))
+                except SSLError as sl:
+                    error("SSL error received, waiting 30 seconds before retrying")
+                    time.sleep(30)
+                    response = requests.get(page_token, auth=("api", api_key))
                 try:
                     data = response.json()
                 except json.decoder.JSONDecodeError:
