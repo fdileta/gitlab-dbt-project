68eda29202a16f8549f0d9bf0b26b831c4c6b268
Paul Armstrong <parmstrong@gitlab.com>
2022-11-08 09:58:49 +0200

    Update select table, add triple quotes.

--- extract/pmg/src/execute.py
+++ extract/pmg/src/execute.py
@@ -14,71 +14,73 @@ config_dict = env.copy()
 
 def get_pmg_reporting_data_query(start_date: str, end_date: str) -> str:
     return (
-        f"SELECT "
-        f"  date,"
-        f"  utm_medium,"
-        f"  utm_source,"
-        f"  utm_campaign,"
-        f"  campaign_code,"
-        f"  geo,"
-        f"  targeting,"
-        f"  ad_unit,"
-        f"  br_nb,"
-        f"  match_unit,"
-        f"  content,"
-        f"  team,"
-        f"  budget,"
-        f"  sales_segment,"
-        f"  data_source,"
-        f"  impressions,"
-        f"  clicks,"
-        f"  conversions,"
-        f"  cost,"
-        f"  ga_conversions,"
-        f"  sends, "
-        f"  opens, "
-        f"  inquiries, "
-        f"  mqls, "
-        f"  linear_sao, "
-        f"  campaign_code_type, "
-        f"  content_type "
-        f"FROM "
-        f"  `pmg-datawarehouse.gitlab.reporting_data`"
-        f"  WHERE date >= '{end_date}' and date < '{start_date}'"
+        f"""
+        SELECT 
+          date,
+          utm_medium,
+          utm_source,
+          utm_campaign,
+          campaign_code,
+          geo,
+          targeting,
+          ad_unit,
+          br_nb,
+          match_unit,
+          content,
+          team,
+          budget,
+          sales_segment,
+          data_source,
+          impressions,
+          clicks,
+          conversions,
+          cost,
+          ga_conversions,
+          sends, 
+          opens, 
+          inquiries, 
+          mqls, 
+          linear_sao, 
+          campaign_code_type, 
+          content_type 
+        FROM 
+          `pmg-datawarehouse.gitlab_analytics.cross_channel`
+          WHERE date >= '{end_date}' and date < '{start_date}'
+        """
     )
 
 
 def write_date_json(date: str, df: DataFrame) -> str:
-    """Just here so we can log in the list comprehension"""
-    file_name = f"pmg_reporting_data_{date}.json"
-    print(f"Writing file {file_name}")
+    Just here so we can log in the list comprehension
+    file_name = fpmg_reporting_data_{date}.json
+    print(fWriting file {file_name})
 
-    df.to_json(file_name, orient="records", date_format="iso")
+    df.to_json(file_name, orient=records, date_format=iso)
 
-    print(f"{file_name} written")
+    print(f{file_name} written)
 
     return file_name
 
 
-if __name__ == "__main__":
+if __name__ == __main__:
 
     bq = BigQueryClient()
 
     # Substringing cause their only needed for string operations in the next function
-    start_time = config_dict["START_TIME"][0:10]
-    end_time = config_dict["END_TIME"][0:10]
+    start_time = config_dict[START_TIME][0:10]
+    end_time = config_dict[END_TIME][0:10]
 
-    snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
+    snowflake_engine = snowflake_engine_factory(config_dict, LOADER)
 
     sql_statement = get_pmg_reporting_data_query(start_time, end_time)
     # Groups by date so we can create a file for each day
-    df_by_date = bq.get_dataframe_from_sql(sql_statement).groupby("date")
+    df_by_date = bq.get_dataframe_from_sql(sql_statement).groupby(date)
 
     written_files = [write_date_json(date, df) for date, df in df_by_date]
 
     [
         snowflake_stage_load_copy_remove(
-            file_name, "pmg.pmg_load", "pmg.paid_digital", snowflake_engine
+            file_name, pmg.pmg_load, pmg.paid_digital, snowflake_engine
         )
         for file_name in written_files
     ]
