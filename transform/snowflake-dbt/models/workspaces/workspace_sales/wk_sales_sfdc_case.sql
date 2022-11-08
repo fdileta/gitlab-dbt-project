891928fc56c8eaf69bd09d5e3e0518afc5684601
Justin Wong <juwong@gitlab.com>
2022-11-08 09:59:58 -0500

    initial case model

---
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/wk_sales_sfdc_case.sql
@@ -0,0 +1,63 @@
+{{ config(alias='sfdc_case') }}
+
+
+WITH source AS (
+
+  SELECT *
+  FROM {{ source('salesforce', 'case') }}
+
+),
+renamed AS (
+    select
+    id AS case_id,
+    account_owner__c AS account_owner,
+    accountid AS account_id,
+    businesshoursid AS business_hours_id,
+    casenumber AS case_number,
+    closeddate AS closed_date,
+    comments,
+    contactemail AS contact_email,
+    contactfax AS contact_fax,
+    contactid AS contact_id,
+    contactmobile AS contact_mobile,
+    contactphone AS contact_phone,
+    description,
+    from_chatter_feed_id__c AS from_chatter_feed_id,
+    isclosed AS is_closed,
+    isclosedoncreate AS is_closed_on_create,
+    isescalated AS is_escalated,
+    lastreferenceddate AS last_referenced_date,
+    lastvieweddate AS last_viewed_date,
+    masterrecordid AS master_record_id,
+    origin,
+    ownerid AS owner_id,
+    parentid AS parent_id,
+    priority,
+    reason,
+    recordtypeid AS record_type_id,
+    resolution_action__c AS resolution_action,
+    sourceid AS source_id,
+    status,
+    subject,
+    suppliedcompany AS supplied_company,
+    suppliedemail AS supplied_email,
+    suppliedname AS supplied_name,
+    suppliedphone AS supplied_phone,
+    type AS case_type,
+
+    -- metadata
+    createdbyid AS created_by_id,
+    createddate AS created_date,
+    isdeleted AS is_deleted,
+    lastmodifiedbyid AS last_modified_by_id,
+    lastmodifieddate AS last_modified_date,
+    CONVERT_TIMEZONE(
+      'America/Los_Angeles', CURRENT_TIMESTAMP()
+    ) AS _last_dbt_run,
+    systemmodstamp
+
+from source
+
+)
+SELECT *
+FROM final
