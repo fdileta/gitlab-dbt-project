0c4831a5a85ed02db157386c34eb91cf78bddffe
Paul Armstrong <parmstrong@gitlab.com>
2022-11-08 10:00:20 +0200

    Return missing quotes.

--- extract/pmg/src/execute.py
+++ extract/pmg/src/execute.py
@@ -51,36 +51,36 @@ def get_pmg_reporting_data_query(start_date: str, end_date: str) -> str:
 
 
 def write_date_json(date: str, df: DataFrame) -> str:
-    Just here so we can log in the list comprehension
-    file_name = fpmg_reporting_data_{date}.json
-    print(fWriting file {file_name})
+    """Just here so we can log in the list comprehension"""
+    file_name = f"pmg_reporting_data_{date}.json"
+    print(f"Writing file {file_name}")
 
-    df.to_json(file_name, orient=records, date_format=iso)
+    df.to_json(file_name, orient="records", date_format="iso")
 
-    print(f{file_name} written)
+    print(f"{file_name} written")
 
     return file_name
 
 
-if __name__ == __main__:
+if __name__ == "__main__":
 
     bq = BigQueryClient()
 
     # Substringing cause their only needed for string operations in the next function
-    start_time = config_dict[START_TIME][0:10]
-    end_time = config_dict[END_TIME][0:10]
+    start_time = config_dict["START_TIME"][0:10]
+    end_time = config_dict["END_TIME"][0:10]
 
-    snowflake_engine = snowflake_engine_factory(config_dict, LOADER)
+    snowflake_engine = snowflake_engine_factory(config_dict, "LOADER")
 
     sql_statement = get_pmg_reporting_data_query(start_time, end_time)
     # Groups by date so we can create a file for each day
-    df_by_date = bq.get_dataframe_from_sql(sql_statement).groupby(date)
+    df_by_date = bq.get_dataframe_from_sql(sql_statement).groupby("date")
 
     written_files = [write_date_json(date, df) for date, df in df_by_date]
 
     [
         snowflake_stage_load_copy_remove(
-            file_name, pmg.pmg_load, pmg.paid_digital, snowflake_engine
+            file_name, "pmg.pmg_load", "pmg.paid_digital", snowflake_engine
         )
         for file_name in written_files
     ]
