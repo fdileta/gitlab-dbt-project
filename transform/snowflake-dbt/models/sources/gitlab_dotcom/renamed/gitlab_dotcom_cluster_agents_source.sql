21a38131e756665b7a13336783cf8ecb061d5641
Jean Peguero <jpeguero@gitlab.com>
2022-11-16 11:58:19 +0100

    Create source model for cluster agents table

---
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_cluster_agents_source.sql
@@ -0,0 +1,21 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ ref('gitlab_dotcom_cluster_agents_dedupe_source') }}
+
+), renamed AS (
+
+    SELECT
+
+      id::NUMBER                                               AS cluster_agent_id,
+      project_id::NUMBER                                       AS project_id,
+      name::VARCHAR                                            AS cluster_agent_name,
+      created_at::TIMESTAMP                                    AS created_at,
+      updated_at::TIMESTAMP                                    AS updated_at
+
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
