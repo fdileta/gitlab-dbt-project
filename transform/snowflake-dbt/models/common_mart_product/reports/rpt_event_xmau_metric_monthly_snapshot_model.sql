9f6c4ef6ce9fb1b767a3cacd0eca28cd8afbf0e4
tpoole1 <tpoole@gmail.com>
2022-11-16 15:25:08 -0500

    dim_user_snapshot_bottom_up

---
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_event_xmau_metric_monthly_snapshot_model.sql
@@ -0,0 +1,55 @@
+{{ config(
+    materialized='incremental',
+    unique_key='rpt_event_xmau_metric_monthly_snapshot_id',
+    tags=["edm_snapshot", "product"]
+) }}
+
+
+WITH snapshot_dates AS (
+
+    SELECT *
+    FROM {{ ref('dim_date') }}
+    WHERE date_actual >= '2022-11-10'
+    AND date_actual <= CURRENT_DATE {% if is_incremental() %}
+
+    -- this filter will only be applied on an incremental run
+    AND date_id > (SELECT max(snapshot_id) FROM {{ this }})
+
+    {% endif %}
+
+), rpt_event_xmau_metric_monthly AS (
+
+    SELECT *
+    FROM {{ ref('rpt_event_xmau_metric_monthly_snapshot') }}
+
+), rpt_event_xmau_metric_monthly_spined AS (
+
+    SELECT
+      snapshot_dates.date_id     AS snapshot_id,
+      snapshot_dates.date_actual AS snapshot_date,
+      rpt_event_xmau_metric_monthly.*
+    FROM rpt_event_xmau_metric_monthly
+    INNER JOIN snapshot_dates
+    ON snapshot_dates.date_actual >= rpt_event_xmau_metric_monthly.dbt_valid_from
+    AND snapshot_dates.date_actual < {{ coalesce_to_infinity('rpt_event_xmau_metric_monthly.dbt_valid_to') }}
+
+), final AS (
+
+     SELECT
+       {{ dbt_utils.surrogate_key(['snapshot_id', 'primary_key']) }} AS rpt_event_xmau_metric_monthly_snapshot_id,
+       *
+     FROM rpt_event_xmau_metric_monthly_spined
+
+)
+
+
+SELECT *
+FROM final
+
+{{ dbt_audit(
+    cte_ref="results",
+    created_by="@snalamaru",
+    updated_by="@snalamaru",
+    created_date="2022-11-03",
+    updated_date="2022-11-03"
+) }}
