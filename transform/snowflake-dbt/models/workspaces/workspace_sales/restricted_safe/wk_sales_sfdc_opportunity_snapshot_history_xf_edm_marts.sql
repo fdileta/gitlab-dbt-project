bf7f61a2b0f1b6e4a7a8d2a158852da44d75321f
Jueun Kim <jkim@gitlab.com>
2022-11-16 13:29:32 +0100

    refactoring wk sales models

--- transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_sales_sfdc_opportunity_snapshot_history_xf_edm_marts.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_sales/restricted_safe/wk_sales_sfdc_opportunity_snapshot_history_xf_edm_marts.sql
@@ -497,7 +497,9 @@ WITH date_details AS (
       upa.account_demographics_geo                               AS upa_demographics_geo,
       upa.account_demographics_region                            AS upa_demographics_region,
       upa.account_demographics_area                              AS upa_demographics_area,
-      upa.account_demographics_territory                         AS upa_demographics_territory
+      upa.account_demographics_territory                         AS upa_demographics_territory,
+
+      opportunity_owner.is_rep_flag
 
       
     FROM sfdc_opportunity_snapshot_history AS opp_snapshot
