94ae346b3870ba1c4149c23289c4a23b236fe521
Jean Peguero <jpeguero@gitlab.com>
2022-11-21 15:43:53 +0100

    Add plan_id to prep cluster agent model

--- transform/snowflake-dbt/models/common_prep/prep_cluster_agent.sql
+++ transform/snowflake-dbt/models/common_prep/prep_cluster_agent.sql
@@ -4,8 +4,9 @@
 
 {{ simple_cte([
   ('cluster_agents','gitlab_dotcom_cluster_agents_source'),
-    ('prep_project', 'prep_project'),
-    ('dim_date', 'dim_date')
+  ('prep_project', 'prep_project'),
+  ('dim_date', 'dim_date'),
+  ('dim_namespace_plan_hist', 'dim_namespace_plan_hist')
   ])
 }}
 
@@ -16,14 +17,19 @@
       prep_project.dim_project_id,
       prep_project.dim_namespace_id,
       prep_project.ultimate_parent_namespace_id,
-      cluster_agents.created_by_user_id           AS dim_user_id,
-      dim_date.date_id                            AS created_date_id,
+      cluster_agents.created_by_user_id                           AS dim_user_id,
+      IFNULL(dim_namespace_plan_hist.dim_plan_id, 34)             AS dim_plan_id,
+      dim_date.date_id                                            AS created_date_id,
       cluster_agents.cluster_agent_name,
       cluster_agents.created_at,
       cluster_agents.updated_at
     FROM cluster_agents
     LEFT JOIN prep_project
       ON cluster_agents.project_id = prep_project.dim_project_id
+    LEFT JOIN dim_namespace_plan_hist 
+      ON prep_project.ultimate_parent_namespace_id = dim_namespace_plan_hist.dim_namespace_id
+      AND cluster_agents.created_at >= dim_namespace_plan_hist.valid_from
+      AND cluster_agents.created_at < COALESCE(dim_namespace_plan_hist.valid_to, '2099-01-01')
     LEFT JOIN dim_date 
       ON cluster_agents.created_at::DATE = dim_date.date_day
       
