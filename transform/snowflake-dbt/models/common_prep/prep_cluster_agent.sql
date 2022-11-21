9ef9ea0ec0b6791f6e9b6eeee2fe7cc0f73365a6
Jean Peguero <jpeguero@gitlab.com>
2022-11-21 14:13:10 +0100

    Addd created_date_id to cluster_agent model

--- transform/snowflake-dbt/models/common_prep/prep_cluster_agent.sql
+++ transform/snowflake-dbt/models/common_prep/prep_cluster_agent.sql
@@ -4,7 +4,8 @@
 
 {{ simple_cte([
   ('cluster_agents','gitlab_dotcom_cluster_agents_source'),
-    ('prep_project', 'prep_project')
+    ('prep_project', 'prep_project'),
+    ('dim_date', 'dim_date')
   ])
 }}
 
@@ -15,13 +16,16 @@
       prep_project.dim_project_id,
       prep_project.dim_namespace_id,
       prep_project.ultimate_parent_namespace_id,
-      cluster_agents.created_by_user_id AS dim_user_id,
+      cluster_agents.created_by_user_id           AS dim_user_id,
+      dim_date.date_id                            AS created_date_id,
       cluster_agents.cluster_agent_name,
       cluster_agents.created_at,
       cluster_agents.updated_at
     FROM cluster_agents
     LEFT JOIN prep_project
       ON cluster_agents.project_id = prep_project.dim_project_id
+    LEFT JOIN dim_date 
+      ON cluster_agents.created_at::DATE = dim_date.date_day
       
 )
 
