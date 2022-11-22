ab8244b93ac37114a02f950bd2d36ee65eb2dbba
Lis Vinueza <lvinueza@gitlab.com>
2022-11-22 13:31:55 -0500

    Remove parent fields and CTE

--- transform/snowflake-dbt/macros/sfdc/sfdc_account_fields.sql
+++ transform/snowflake-dbt/macros/sfdc/sfdc_account_fields.sql
@@ -87,55 +87,6 @@ WITH map_merged_crm_account AS (
     SELECT *
     FROM {{ ref('sfdc_record_type') }}
 
-), ultimate_parent_account AS (
-
-    SELECT
-      {%- if model_type == 'live' %}
-
-      {%- elif model_type == 'snapshot' %}
-      crm_account_snapshot_id,
-      snapshot_id,
-      {%- endif %}
-      account_id,
-      account_name,
-      billing_country,
-      billing_country_code,
-      df_industry,
-      industry,
-      sub_industry,
-      account_owner_team,
-      tsp_territory,
-      tsp_region,
-      tsp_sub_region,
-      tsp_area,
-      gtm_strategy,
-      tsp_account_employees,
-      tsp_max_family_employees,
-      account_demographics_sales_segment,
-      account_demographics_geo,
-      account_demographics_region,
-      account_demographics_area,
-      account_demographics_territory,
-      account_demographics_employee_count,
-      account_demographics_max_family_employee,
-      account_demographics_upa_country,
-      account_demographics_upa_state,
-      account_demographics_upa_city,
-      account_demographics_upa_street,
-      account_demographics_upa_postal_code,
-      created_date,
-      zi_technologies,
-      zoom_info_website,
-      zoom_info_company_other_domains,
-      zoom_info_dozisf_zi_id,
-      zoom_info_parent_company_zi_id,
-      zoom_info_parent_company_name,
-      zoom_info_ultimate_parent_company_zi_id,
-      zoom_info_ultimate_parent_company_name,
-      zoom_info_total_funding
-    FROM sfdc_account
-    WHERE account_id = ultimate_parent_account_id
-
 ), pte_scores AS (
 
     SELECT 
@@ -204,42 +155,8 @@ WITH map_merged_crm_account AS (
       technical_account_manager.name                                      AS technical_account_manager,
 
       ----ultimate parent crm account info
-      ultimate_parent_account.account_name                                AS parent_crm_account_name,
-      {{ sales_segment_cleaning('sfdc_account.ultimate_parent_sales_segment') }}
-                                                                          AS parent_crm_account_sales_segment,
-      ultimate_parent_account.billing_country                             AS parent_crm_account_billing_country,
-      ultimate_parent_account.billing_country_code                        AS parent_crm_account_billing_country_code,
-      ultimate_parent_account.industry                                    AS parent_crm_account_industry,
-      ultimate_parent_account.sub_industry                                AS parent_crm_account_sub_industry,
-      sfdc_account.parent_account_industry_hierarchy                      AS parent_crm_account_industry_hierarchy,
-      ultimate_parent_account.account_owner_team                          AS parent_crm_account_owner_team,
-      ultimate_parent_account.tsp_territory                               AS parent_crm_account_sales_territory,
-      ultimate_parent_account.tsp_region                                  AS parent_crm_account_tsp_region,
-      ultimate_parent_account.tsp_sub_region                              AS parent_crm_account_tsp_sub_region,
-      ultimate_parent_account.tsp_area                                    AS parent_crm_account_tsp_area,
-      ultimate_parent_account.gtm_strategy                                AS parent_crm_account_gtm_strategy,
-      CASE
-        WHEN LOWER(ultimate_parent_account.gtm_strategy) IN ('account centric', 'account based - net new', 'account based - expand') THEN 'Focus Account'
-        ELSE 'Non - Focus Account'
-      END                                                                 AS parent_crm_account_focus_account,
-      ultimate_parent_account.tsp_account_employees                       AS parent_crm_account_tsp_account_employees,
-      ultimate_parent_account.tsp_max_family_employees                    AS parent_crm_account_tsp_max_family_employees,
-      CASE
-         WHEN ultimate_parent_account.tsp_max_family_employees > 2000 THEN 'Employees > 2K'
-         WHEN ultimate_parent_account.tsp_max_family_employees <= 2000 AND ultimate_parent_account.tsp_max_family_employees > 1500 THEN 'Employees > 1.5K'
-         WHEN ultimate_parent_account.tsp_max_family_employees <= 1500 AND ultimate_parent_account.tsp_max_family_employees > 1000  THEN 'Employees > 1K'
-         ELSE 'Employees < 1K'
-      END                                                                AS parent_crm_account_employee_count_band,
-      ultimate_parent_account.created_date                               AS parent_crm_account_created_date,
-      ultimate_parent_account.zi_technologies                            AS parent_crm_account_zi_technologies,
-      ultimate_parent_account.zoom_info_website                          AS parent_crm_account_zoom_info_website,
-      ultimate_parent_account.zoom_info_company_other_domains            AS parent_crm_account_zoom_info_company_other_domains,
-      ultimate_parent_account.zoom_info_dozisf_zi_id                     AS parent_crm_account_zoom_info_dozisf_zi_id,
-      ultimate_parent_account.zoom_info_parent_company_zi_id             AS parent_crm_account_zoom_info_parent_company_zi_id,
-      ultimate_parent_account.zoom_info_parent_company_name              AS parent_crm_account_zoom_info_parent_company_name,
-      ultimate_parent_account.zoom_info_ultimate_parent_company_zi_id    AS parent_crm_account_zoom_info_ultimate_parent_company_zi_id,
-      ultimate_parent_account.zoom_info_ultimate_parent_company_name     AS parent_crm_account_zoom_info_ultimate_parent_company_name,
-      ultimate_parent_account.zoom_info_total_funding                    AS parent_crm_account_zoom_info_total_funding,
+      
+      sfdc_account.parent_account_industry                                AS parent_crm_account_industry,
 
       --descriptive attributes
       sfdc_account.account_name                                           AS crm_account_name,
@@ -261,8 +178,6 @@ WITH map_merged_crm_account AS (
         ELSE 'Non - Focus Account'
       END                                                                 AS crm_account_focus_account,
       sfdc_account.account_owner_user_segment                             AS crm_account_owner_user_segment,
-      sfdc_account.tsp_account_employees                                  AS crm_account_tsp_account_employees,
-      sfdc_account.tsp_max_family_employees                               AS crm_account_tsp_max_family_employees,
       sfdc_account.billing_country                                        AS crm_account_billing_country,
       sfdc_account.billing_country_code                                   AS crm_account_billing_country_code,
       sfdc_account.account_type                                           AS crm_account_type,
@@ -270,24 +185,11 @@ WITH map_merged_crm_account AS (
       sfdc_account.sub_industry                                           AS crm_account_sub_industry,
       sfdc_account.account_owner                                          AS crm_account_owner,
       sfdc_account.account_owner_team                                     AS crm_account_owner_team,
-      sfdc_account.tsp_territory                                          AS crm_account_sales_territory,
-      sfdc_account.tsp_region                                             AS crm_account_tsp_region,
-      sfdc_account.tsp_sub_region                                         AS crm_account_tsp_sub_region,
-      sfdc_account.tsp_area                                               AS crm_account_tsp_area,
-      sfdc_account.tsp_max_hierarchy_sales_segment                        AS tsp_max_hierarchy_sales_segment,
-      CASE
-         WHEN sfdc_account.tsp_max_family_employees > 2000 THEN 'Employees > 2K'
-         WHEN sfdc_account.tsp_max_family_employees <= 2000 AND sfdc_account.tsp_max_family_employees > 1500 THEN 'Employees > 1.5K'
-         WHEN sfdc_account.tsp_max_family_employees <= 1500 AND sfdc_account.tsp_max_family_employees > 1000  THEN 'Employees > 1K'
-         ELSE 'Employees < 1K'
-      END                                                                 AS crm_account_employee_count_band,
-      sfdc_account.tsp_account_employees,
-      sfdc_account.tsp_max_family_employees,
-      sfdc_account.partner_vat_tax_id,
-      sfdc_account.account_manager,
-      sfdc_account.business_development_rep,
-      sfdc_account.dedicated_service_engineer,
-      sfdc_account.account_tier,
+      sfdc_account.partner_vat_tax_id                                     AS partner_vat_tax_id,
+      sfdc_account.account_manager                                        AS account_manager,
+      sfdc_account.business_development_rep                               AS business_development_rep,
+      sfdc_account.dedicated_service_engineer                             AS dedicated_service_engineer,
+      sfdc_account.account_tier                                           AS account_tier,
       sfdc_account.license_utilization,
       sfdc_account.support_level,
       sfdc_account.named_account,
@@ -334,14 +236,14 @@ WITH map_merged_crm_account AS (
       sfdc_account.zoom_info_ultimate_parent_company_name                 AS crm_account_zoom_info_ultimate_parent_company_name,
       sfdc_account.zoom_info_number_of_developers                         AS crm_account_zoom_info_number_of_developers,
       sfdc_account.zoom_info_total_funding                                AS crm_account_zoom_info_total_funding,
-      sfdc_account.forbes_2000_rank,
-      sfdc_account.parent_account_industry_hierarchy,
-      sfdc_account.sales_development_rep,
-	  sfdc_account.admin_manual_source_number_of_employees,
-      sfdc_account.admin_manual_source_account_address,      
+      sfdc_account.forbes_2000_rank                                       AS forbes_2000_rank,
+      sfdc_account.parent_account_industry_hierarchy                      AS parent_account_industry_hierarchy,
+      sfdc_account.sales_development_rep                                  AS sales_development_rep,
+	  sfdc_account.admin_manual_source_number_of_employees                AS admin_manual_source_number_of_employees, 
+      sfdc_account.admin_manual_source_account_address                    AS admin_manual_source_account_address,      
 
       --degenerative dimensions
-      sfdc_account.is_sdr_target_account,
+      sfdc_account.is_sdr_target_account                                  AS is_sdr_target_account,
       IFF(sfdc_record_type.record_type_label = 'Partner'
           AND sfdc_account.partner_type IN ('Alliance', 'Channel')
           AND sfdc_account.partner_status = 'Authorized',
@@ -448,10 +350,10 @@ WITH map_merged_crm_account AS (
       {{ get_date_id('sfdc_account.customer_since_date') }}               AS customer_since_date_id,
       sfdc_account.customer_since_date,
       {{ get_date_id('sfdc_account.next_renewal_date') }}                 AS next_renewal_date_id,
-      sfdc_account.next_renewal_date,
+      sfdc_account.next_renewal_date                                      AS next_renewal_date,
 
       --measures
-      sfdc_account.count_active_subscription_charges,
+      sfdc_account.count_active_subscription_charges                      ,
       sfdc_account.count_active_subscriptions,
       sfdc_account.count_billing_accounts,
       sfdc_account.count_licensed_users,
