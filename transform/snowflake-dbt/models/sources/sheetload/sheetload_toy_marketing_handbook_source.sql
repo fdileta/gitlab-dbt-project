4167dbfef0a75d48b5c870e9484e0788176c6ac5
Justin Wong <juwong@gitlab.com>
2022-11-21 20:14:55 -0500

    cast source model

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_toy_marketing_handbook_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_toy_marketing_handbook_source.sql
@@ -1,11 +1,26 @@
 WITH source AS (
 
-    SELECT *
-    FROM {{ source('sheetload', 'toy_marketing_handbook') }}
+  SELECT
+    age::int AS age,
+    job,
+    marital,
+    education,
+    default,
+    balance::int AS balance,
+    housing,
+    loan,
+    contact,
+    day::int AS day,
+    month,
+    duration::int AS duration,
+    campaign::int AS campaign,
+    pdays::int AS pdays,
+    previous::int AS previous,
+    poutcome,
+    y
+  FROM {{ source('sheetload', 'toy_marketing_handbook') }}
 
 )
 
 SELECT *
 FROM source
-
-
