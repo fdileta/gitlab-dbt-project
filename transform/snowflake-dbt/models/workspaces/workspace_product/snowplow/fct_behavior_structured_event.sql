9ec39899bbf23c3201cf38fdb9cdb496d48204fe
Chris Sharp <csharp@gitlab.com>
2022-11-24 14:18:56 +0000

    use unnested events directly and generate keys

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_structured_event.sql
@@ -4,16 +4,74 @@
     unique_key='behavior_structured_event_pk',
     full_refresh= only_force_full_refresh(),
     on_schema_change='sync_all_columns',
-    post_hook=["{{ rolling_window_delete('behavior_at','month',24) }}"]
+    post_hook=["{{ rolling_window_delete('behavior_at','month',25) }}"]
 
   )
 
 }}
-{{ 
-    simple_cte([
-    ('prep_snowplow_structured_event_all_source', 'prep_snowplow_structured_event_all_source')
-    ])
-}}
+
+WITH structured_event_renamed AS (
+    SELECT
+    
+      event_id::VARCHAR                         AS event_id,
+      v_tracker::VARCHAR                        AS v_tracker,
+      se_action::VARCHAR                        AS event_action,
+      se_category::VARCHAR                      AS event_category,
+      se_label::VARCHAR                         AS event_label,
+      se_property::VARCHAR                      AS event_property,
+      se_value::VARCHAR                         AS event_value,
+      event::VARCHAR                            AS event,
+      event_name::VARCHAR                       AS event_name,
+      TRY_PARSE_JSON(contexts)::VARIANT         AS contexts,
+      dvce_created_tstamp::TIMESTAMP            AS dvce_created_tstamp,
+      derived_tstamp::TIMESTAMP                 AS derived_tstamp,
+      collector_tstamp::TIMESTAMP               AS collector_tstamp,
+      user_id::VARCHAR                          AS user_custom_id,
+      domain_userid::VARCHAR                    AS user_snowplow_domain_id,
+      network_userid::VARCHAR                   AS user_snowplow_crossdomain_id,
+      domain_sessionid::VARCHAR                 AS session_id,
+      domain_sessionidx::INT                    AS session_index,
+      platform::VARCHAR                         AS platform,
+      (page_urlhost || page_urlpath)::VARCHAR   AS page_url,
+      page_urlscheme::VARCHAR                   AS page_url_scheme,
+      page_urlhost::VARCHAR                     AS page_url_host,
+      page_urlpath::VARCHAR                     AS page_url_path,
+      page_urlfragment::VARCHAR                 AS page_url_fragment,
+      mkt_medium::VARCHAR                       AS marketing_medium,
+      mkt_source::VARCHAR                       AS marketing_source,
+      mkt_term::VARCHAR                         AS marketing_term,
+      mkt_content::VARCHAR                      AS marketing_content,
+      mkt_campaign::VARCHAR                     AS marketing_campaign,
+      app_id::VARCHAR                           AS app_id,
+      br_family::VARCHAR                        AS browser_name,
+      br_name::VARCHAR                          AS browser_major_version,
+      br_version::VARCHAR                       AS browser_minor_version,
+      os_family::VARCHAR                        AS os,
+      os_name::VARCHAR                          AS os_name,
+      br_lang::VARCHAR                          AS browser_language,
+      os_manufacturer::VARCHAR                  AS os_manufacturer,
+      os_timezone::VARCHAR                      AS os_timezone,
+      br_renderengine::VARCHAR                  AS browser_engine,
+      dvce_type::VARCHAR                        AS device_type,
+      dvce_ismobile::BOOLEAN                    AS device_is_mobile,
+      gsc_environment                           AS gsc_environment,
+      gsc_extra                                 AS gsc_extra,
+      gsc_namespace_id                          AS gsc_namespace_id,
+      gsc_plan                                  AS gsc_plan,
+      gsc_google_analytics_client_id            AS gsc_google_analytics_client_id,
+      gsc_project_id                            AS gsc_project_id,
+      gsc_pseudonymized_user_id                 AS gsc_pseudonymized_user_id,
+      gsc_source                                AS gsc_source
+
+    FROM {{ ref('prep_snowplow_unnested_events_all') }}
+    WHERE event = 'struct'
+      AND derived_tstamp > DATEADD(MONTH, -25, CURRENT_DATE)
+    {% if is_incremental() %}
+
+      AND derived_tstamp > (SELECT MAX(behavior_at) FROM {{ this }})
+
+    {% endif %}
+)
 
 , dim_behavior_website_page AS (
 
@@ -24,48 +82,12 @@
       dim_behavior_website_page.app_id
     FROM {{ ref('dim_behavior_website_page') }}
 
-), dim_behavior_browser AS (
-
-    SELECT 
-      dim_behavior_browser.dim_behavior_browser_sk,
-      dim_behavior_browser.browser_name,
-      dim_behavior_browser.browser_major_version,
-      dim_behavior_browser.browser_minor_version,
-      dim_behavior_browser.browser_language
-    FROM {{ ref('dim_behavior_browser') }}
-
-), dim_behavior_operating_system AS (
-
-    SELECT 
-      dim_behavior_operating_system.dim_behavior_operating_system_sk,
-      dim_behavior_operating_system.os_name,
-      dim_behavior_operating_system.os_timezone
-    FROM {{ ref('dim_behavior_operating_system') }}
-
-), dim_behavior_event AS (
-
-    SELECT
-      dim_behavior_event_sk,
-      event,
-      event_name,
-      platform,
-      environment,
-      event_category,
-      event_action,
-      event_label,
-      event_property
-    FROM {{ ref('dim_behavior_event') }}
-    WHERE event = 'struct'
-
 ), structured_events_w_clean_url AS (
 
     SELECT 
-      prep_snowplow_structured_event_all_source.*,
-      'struct'                                                                    AS event,
-      'event'                                                                     AS event_name,
-      {{ clean_url('prep_snowplow_structured_event_all_source.page_url_path') }}  AS clean_url_path
-    FROM prep_snowplow_structured_event_all_source
-    WHERE derived_tstamp > DATEADD(MONTH, -25, CURRENT_DATE)
+      structured_event_renamed.*,
+      {{ clean_url('structured_event_renamed.page_url_path') }}  AS clean_url_path
+    FROM structured_event_renamed
 
 ), structured_events_w_dim AS (
 
@@ -76,15 +98,15 @@
 
       -- Foreign Keys
       dim_behavior_website_page.dim_behavior_website_page_sk,
-      dim_behavior_browser.dim_behavior_browser_sk,
-      dim_behavior_operating_system.dim_behavior_operating_system_sk,
-      structured_events_w_clean_url.gsc_namespace_id AS dim_namespace_id,
-      structured_events_w_clean_url.gsc_project_id AS dim_project_id,
-      {{ dbt_utils.surrogate_key(['event', 'event_name', 'platform', 'environment', 'event_category', 'event_action', 'event_label', 'event_property']) }} AS dim_behavior_event_sk,
+      {{ dbt_utils.surrogate_key(['browser_name', 'browser_major_version', 'browser_minor_version', 'browser_language']) }} AS dim_behavior_browser_sk,
+      {{ dbt_utils.surrogate_key(['os_name', 'os_timezone']) }}                                                             AS dim_behavior_operating_system_sk,
+      structured_events_w_clean_url.gsc_namespace_id                                                                        AS dim_namespace_id,
+      structured_events_w_clean_url.gsc_project_id                                                                          AS dim_project_id,
+      {{ dbt_utils.surrogate_key(['event', 'event_name', 'platform', 'gsc_environment', 'event_category', 'event_action', 'event_label', 'event_property']) }} AS dim_behavior_event_sk,
 
       -- Time Attributes
       structured_events_w_clean_url.dvce_created_tstamp,
-      structured_events_w_clean_url.derived_tstamp AS behavior_at,
+      structured_events_w_clean_url.derived_tstamp                                     AS behavior_at,
 
       -- Degenerate Dimensions (Event Attributes)
       structured_events_w_clean_url.v_tracker,
@@ -112,18 +134,6 @@
       ON structured_events_w_clean_url.clean_url_path = dim_behavior_website_page.clean_url_path
         AND structured_events_w_clean_url.page_url_host = dim_behavior_website_page.page_url_host
         AND structured_events_w_clean_url.app_id = dim_behavior_website_page.app_id
-    LEFT JOIN dim_behavior_browser
-      ON structured_events_w_clean_url.browser_name = dim_behavior_browser.browser_name
-        AND structured_events_w_clean_url.browser_major_version = dim_behavior_browser.browser_major_version
-        AND structured_events_w_clean_url.browser_minor_version = dim_behavior_browser.browser_minor_version
-        AND structured_events_w_clean_url.browser_language = dim_behavior_browser.browser_language
-    LEFT JOIN dim_behavior_operating_system
-      ON structured_events_w_clean_url.os_name = dim_behavior_operating_system.os_name
-        AND structured_events_w_clean_url.os_timezone = dim_behavior_operating_system.os_timezone
-    WHERE DATE_TRUNC(MONTH, behavior_at) >= DATEADD(MONTH, -24, DATE_TRUNC(MONTH, CURRENT_DATE))
-    {% if is_incremental() %}
-      AND behavior_at >= (SELECT MAX(behavior_at) FROM {{ this }})
-    {% endif %}
 
 )
 
