c05d9f878999720bbeb2bf235b028b5e5bd5caa7
Lis Vinueza <lvinueza@gitlab.com>
2022-11-24 11:47:58 -0500

    Remove parent account name

--- transform/snowflake-dbt/models/common_mart_sales/restricted_safe/mart_retention_parent_account.sql
+++ transform/snowflake-dbt/models/common_mart_sales/restricted_safe/mart_retention_parent_account.sql
@@ -107,7 +107,6 @@
     {{ dbt_utils.surrogate_key(['retention_subs.dim_parent_crm_account_id','retention_month']) }}
                                                 AS fct_retention_id,
       retention_subs.dim_parent_crm_account_id  AS dim_parent_crm_account_id,
-      dim_crm_account.crm_account_name          AS parent_crm_account_name,
       retention_month,
       IFF(is_first_day_of_last_month_of_fiscal_year, fiscal_year, NULL)               AS retention_fiscal_year,
       IFF(is_first_day_of_last_month_of_fiscal_quarter, fiscal_quarter_name_fy, NULL) AS retention_fiscal_quarter,
