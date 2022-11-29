cce8d851cfa8a6fc74010615c8c34ecc9cc013ae
ved prakash <vprakash@gitlab.com>
2022-11-29 11:18:37 +0100

    create backup table

--- dags/extract/gitlab_data_deduplication.py
+++ dags/extract/gitlab_data_deduplication.py
@@ -7,7 +7,8 @@ from airflow_utils import (
     DATA_IMAGE,
     clone_repo_cmd,
     gitlab_defaults,
-    slack_failed_task,)
+    slack_failed_task,
+)
 
 from kubernetes_helpers import get_affinity, get_toleration
 from kube_secrets import (
@@ -32,35 +33,34 @@ scd_cmd = f"""
             cd analytics/extract/gitlab_deduplication/ &&
             python main.py deduplication manifest_deduplication/t_gitlab_com_deduplication_table_manifest.yaml 
 """
-default_args={
-        "catchup": False,
-        "depends_on_past": False,
-        "on_failure_callback": slack_failed_task,
-        "owner": "airflow",
-        "retries": 0,
-        "start_date": datetime(2020, 1, 1),
-    }
+default_args = {
+    "catchup": False,
+    "depends_on_past": False,
+    "on_failure_callback": slack_failed_task,
+    "owner": "airflow",
+    "retries": 0,
+    "start_date": datetime(2020, 1, 1),
+}
 
 scd_deduplication_dag = DAG(
-        "scd_deduplication",
-        default_args=default_args,
-        schedule_interval=None
-    )
+    "scd_deduplication", default_args=default_args, schedule_interval=None
+)
 
 scd_deduplication = KubernetesPodOperator(
     **gitlab_defaults,
     image=DATA_IMAGE,
     task_id="scd_table_deduplication_task",
     name="scd_table_deduplication_task",
-    secrets=[SNOWFLAKE_ACCOUNT,
-    SNOWFLAKE_LOAD_PASSWORD,
-    SNOWFLAKE_LOAD_ROLE,
-    SNOWFLAKE_LOAD_USER,
-    SNOWFLAKE_LOAD_WAREHOUSE,],
+    secrets=[
+        SNOWFLAKE_ACCOUNT,
+        SNOWFLAKE_LOAD_PASSWORD,
+        SNOWFLAKE_LOAD_ROLE,
+        SNOWFLAKE_LOAD_USER,
+        SNOWFLAKE_LOAD_WAREHOUSE,
+    ],
     env_vars=pod_env_vars,
     arguments=[scd_cmd],
     affinity=get_affinity(False),
     tolerations=get_toleration(False),
-    dag=scd_deduplication_dag
+    dag=scd_deduplication_dag,
 )
-
--- extract/gitlab_deduplication/main.py
+++ extract/gitlab_deduplication/main.py
@@ -5,10 +5,13 @@ from os import environ as env
 from datetime import datetime
 from fire import Fire
 from typing import Dict
-from gitlabdata.orchestration_utils import (snowflake_engine_factory,
-    query_executor,)
+from gitlabdata.orchestration_utils import (
+    snowflake_engine_factory,
+    query_executor,
+)
 
 snowflake_engine = snowflake_engine_factory(env, "LOADER")
+
 
 def manifest_reader(file_path: str) -> Dict[str, Dict]:
     """
@@ -20,39 +23,72 @@ def manifest_reader(file_path: str) -> Dict[str, Dict]:
 
     return manifest_dict
 
-def build_table_name(table_name:str,table_prefix:str = None,table_suffix:str = None) -> str:
-        if table_prefix is None and table_suffix is None:
-            return table_name
-        elif table_prefix is None and table_suffix is not None:
-            return table_name+table_suffix
-        elif table_prefix is not None and table_suffix is None:
-            return table_prefix+table_name
-        else:
-            return table_prefix+table_name+table_suffix
 
-def create_backup_table(backup_schema_name:str,table_name:str,table_prefix:str,raw_schema:str,raw_database:str) -> bool:
-    table_suffix="_"+datetime.now().strftime("%Y%m%d")
-    if table_prefix:
-        bkp_table_name=build_table_name(table_name,table_prefix,table_suffix)
-        original_table_name=build_table_name(table_name,table_prefix)
+def build_table_name(
+    table_name: str, table_prefix: str = None, table_suffix: str = None
+) -> str:
+    if table_prefix is None and table_suffix is None:
+        return table_name
+    elif table_prefix is None and table_suffix is not None:
+        return table_name + table_suffix
+    elif table_prefix is not None and table_suffix is None:
+        return table_prefix + table_name
     else:
-        bkp_table_name=build_table_name(table_name,table_suffix)
-        original_table_name=build_table_name(table_name)
-    create_backup_table=f"CREATE TABLE {raw_database}.{backup_schema_name}.{bkp_table_name} CLONE {raw_database}.{raw_schema}.{original_table_name};"
-    logging.info(f'create_backup_table')
+        return table_prefix + table_name + table_suffix
+
+def create_table_name(manifest_dict,table_name):
+    table_suffix = "_" + datetime.now().strftime("%Y%m%d")
+    table_prefix = manifest_dict["generic_info"]["table_prefix"]
+
+    if table_prefix:
+        bkp_table_name = build_table_name(table_name, table_prefix, table_suffix)
+        original_table_name = build_table_name(table_name, table_prefix)
+    else:
+        bkp_table_name = build_table_name(table_name, table_suffix)
+        original_table_name = build_table_name(table_name)
+    return bkp_table_name,original_table_name
+
+def create_backup_table(
+    manifest_dict: dict,
+    table_name: str,
+) -> bool:
+    raw_database = manifest_dict["raw_database"]
+    backup_schema_name = manifest_dict["generic_info"]["backup_schema"]
+    raw_schema = manifest_dict["generic_info"]["raw_schema"]
+    bkp_table_name,original_table_name =create_table_name(manifest_dict,table_name)
+    create_backup_table = f"CREATE TABLE {raw_database}.{backup_schema_name}.{bkp_table_name} CLONE {raw_database}.{raw_schema}.{original_table_name};"
+    logging.info(f"create_backup_table")
     query_executor(snowflake_engine, create_backup_table)
     return True
 
+def get_column_list():
+    
+    return True
 
-def deduplicate_scd_tables(manifest_dict: Dict,table_name: str) -> bool:
-        backup_schema_name=manifest_dict["generic_info"]["backup_schema"]
-        backup_retention_policy=manifest_dict["generic_info"]["backup_retention_policy"]
-        table_prefix=manifest_dict["generic_info"]["table_prefix"]
-        raw_schema=manifest_dict["generic_info"]["raw_schema"]
-        raw_database=manifest_dict["raw_database"]
-        create_backup_table(backup_schema_name,table_name,table_prefix,raw_schema,raw_database)
+def create_temp_table(
+    manifest_dict: dict,
+    table_name: str,
+):
+    get_column_list()
+    return True
 
-def main(file_path: str = 't_gitlab_com_scd_advance_metadata_manifest.yml') -> None:
+
+def deduplicate_scd_tables(manifest_dict: Dict, table_name: str) -> bool:
+    """
+    Extract the values from the manifest
+    """
+    table_prefix = manifest_dict["generic_info"]["table_prefix"]
+    raw_schema = manifest_dict["generic_info"]["raw_schema"]
+    raw_database = manifest_dict["raw_database"]
+    # Create backup table
+    create_backup_table(
+        manifest_dict,  table_name
+    )
+    if create_backup_table:
+        create_temp_table(manifest_dict,table_name)
+
+
+def main(file_path: str = "t_gitlab_com_scd_advance_metadata_manifest.yml") -> None:
     """
     Read table name from manifest file and decide if the table exist in the database. Check if the advance metadata column `_task_instance`
     is present in the table.
@@ -65,13 +101,13 @@ def main(file_path: str = 't_gitlab_com_scd_advance_metadata_manifest.yml') -> N
     # Process the manifest
     logging.info(f"Reading manifest at location: {file_path}")
     manifest_dict = manifest_reader(file_path)
-    scd_tables_list=manifest_dict["scd_tables"]
+    scd_tables_list = manifest_dict["scd_tables"]
     # Add raw database name to the manifest
-    manifest_dict.update({'raw_database' : env.copy()["SNOWFLAKE_LOAD_DATABASE"]})
-    #iterate through each table and check if it exist 
+    manifest_dict.update({"raw_database": env.copy()["SNOWFLAKE_LOAD_DATABASE"]})
+    # iterate through each table and check if it exist
     for table in scd_tables_list:
-        logging.info(f'Proceeding with table {table} for deduplication')
-        deduplicate_scd_tables(manifest_dict,table)
+        logging.info(f"Proceeding with table {table} for deduplication")
+        deduplicate_scd_tables(manifest_dict, table)
 
 
 if __name__ == "__main__":
