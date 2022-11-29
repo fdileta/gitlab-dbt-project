23ce4c7092b7f83cbfc4b52eb61303eda2698f14
tpoole1 <tpoole@gmail.com>
2022-11-29 15:41:55 -0500

    Ready for Review

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_user_details_snapshots_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_user_details_snapshots_source.sql
@@ -6,6 +6,16 @@ WITH source AS (
 ), renamed AS (
 
     SELECT
+      CASE COALESCE(registration_objective,-1)
+          WHEN 0 THEN 'basics' 
+          WHEN 1 THEN 'move_repository' 
+          WHEN 2 THEN 'code_storage' 
+          WHEN 3 THEN 'exploring' 
+          WHEN 4 THEN 'ci' 
+          WHEN 5 THEN 'other' 
+          WHEN 6 THEN 'joining_team'
+          WHEN -1 THEN 'Unknown'
+      END AS jobs_to_be_done,
       *
     FROM source
     
