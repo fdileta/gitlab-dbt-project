dde84e764108c09fcd257e7c5f365484a25f0c3c
tpoole1 <tpoole@gmail.com>
2022-11-29 15:41:55 -0500

    Ready for Review

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_users_snapshots_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_users_snapshots_source.sql
@@ -15,7 +15,7 @@ WITH source AS (
       SPLIT_PART(email, '@', 2)::VARCHAR AS email_domain,      
       SPLIT_PART(public_email, '@', 2)::VARCHAR AS public_email_domain,
       IFF(SPLIT_PART(commit_email, '@', 2) = '', NULL, SPLIT_PART(commit_email, '@', 2))::VARCHAR AS commit_email_domain,
-      {{ user_role_mapping(user_role='role') }}::VARCHAR AS role_description,
+      COALESCE({{ user_role_mapping(user_role='role') }}::VARCHAR,'Unknown') AS role_description,
       COALESCE(TO_DATE(last_activity_on)::VARCHAR,'Unknown') AS last_activity_date,              
       COALESCE(TO_DATE(last_sign_in_at)::VARCHAR,'Unknown')  AS last_sign_in_date, 
       admin::BOOLEAN AS is_admin,              
