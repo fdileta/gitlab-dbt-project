1a48d7aaf39daaa6c06467744087de641a6dae07
Chris Sharp <csharp@gitlab.com>
2022-11-30 13:39:59 +0000

    alias fields

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/dim_behavior_browser.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/dim_behavior_browser.sql
@@ -8,21 +8,21 @@ WITH browser_information AS (
   SELECT DISTINCT
     -- surrogate key
     {{ dbt_utils.surrogate_key([
-        'br_family',
-        'br_name',
-        'br_version',
-        'br_lang'
+        'browser_name',
+        'browser_major_version',
+        'browser_minor_version',
+        'browser_language'
         ]) }}                   AS dim_behavior_browser_sk,
 
     -- natural key
-    br_family::VARCHAR          AS browser_name,
-    br_name::VARCHAR            AS browser_major_version,
-    br_version::VARCHAR         AS browser_minor_version,
+    browser_name,
+    browser_major_version,
+    browser_minor_version,
 
     -- attributes
-    br_renderengine::VARCHAR    AS browser_engine,
-    br_lang::VARCHAR            AS browser_language,
-    MAX(derived_tstamp)       AS max_timestamp
+    browser_engine,
+    browser_language,
+    MAX(derived_tstamp)         AS max_timestamp
   FROM {{ ref('prep_snowplow_unnested_events_all') }}
   WHERE true
 
@@ -42,5 +42,5 @@ WITH browser_information AS (
     created_by="@michellecooper",
     updated_by="@chrissharp",
     created_date="2022-09-20",
-    updated_date="2022-10-14"
+    updated_date="2022-11-30"
 ) }}
