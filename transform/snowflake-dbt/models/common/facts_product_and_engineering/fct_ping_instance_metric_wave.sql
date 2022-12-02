42539c2e7d7ff7c4f1da3cb84999f70ce73335a9
Radovan Bacovic <rbacovic@gitlab.com>
2022-12-02 14:38:47 +0100

    Add LICENSE_SHA256 in the WHERE clause along with LICENSE_MD5.

--- transform/snowflake-dbt/models/common/facts_product_and_engineering/fct_ping_instance_metric_wave.sql
+++ transform/snowflake-dbt/models/common/facts_product_and_engineering/fct_ping_instance_metric_wave.sql
@@ -19,7 +19,8 @@
 , fct_ping_instance_metric_with_license  AS (
     SELECT *
     FROM {{ ref('fct_ping_instance_metric') }}
-    WHERE license_md5 IS NOT NULL
+    WHERE (license_md5 IS NOT NULL OR
+           license_sha256 IS NOT NULL)
 )
 
 , final AS (
