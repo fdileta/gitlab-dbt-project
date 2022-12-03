cb3cc2eee3dab66592af0bea812d8c9d23a40181
jjstark <jstark@gitlab.com>
2022-12-03 12:50:52 -0800

    add overwrite

--- pump/pumps.py
+++ pump/pumps.py
@@ -33,19 +33,21 @@ def get_copy_command(model, sensitive, timestamp, inc_start, inc_end, stage, sin
         if single is False:
             target_name = model
             option = "INCLUDE_QUERY_ID"
+            overwrite = ''
         else:
             inc_end = datetime.fromisoformat(inc_end)
             file_stamp = inc_end.strftime("%Y_%m_%d__%H%M%S")
             target_name = f"{model}/{file_stamp}.csv"
             option = "SINGLE"
+            overwrite = 'OVERWRITE = TRUE'
 
         tmp_copy_command = f"""
             COPY INTO @RAW.PUBLIC.{stage}/{target_name}
             FROM ({query} LIMIT 1000000)
             FILE_FORMAT = (TYPE = CSV, NULL_IF = (), FIELD_OPTIONALLY_ENCLOSED_BY = '"', COMPRESSION=NONE)
             HEADER = TRUE
-            OVERWRITE = TRUE
             {option} = TRUE
+            {overwrite}
             ;
         """
 
