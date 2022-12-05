d2b64beded7581044aab8f1b2cb6e62db3760446
Lis Vinueza <lvinueza@gitlab.com>
2022-12-05 16:34:15 +0000

    Join custom dimensions

--- transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session_hit.sql
+++ transform/snowflake-dbt/models/legacy/google_analytics_360/intermediate/ga360_session_hit.sql
@@ -3,7 +3,25 @@ WITH source AS (
 	SELECT *
 	FROM {{ ref('ga360_session_hit_source') }}
 
+), custom_dimensions AS (
+
+	SELECT *
+	FROM {{ ref('ga360_session_custom_dimension_xf') }}
+	
+), joined AS (
+
+	SELECT 
+		source.*,
+		dims.dimension_index,
+		dims.dimension_name
+	FROM source
+	LEFT JOIN custom_dimensions AS dims
+	ON dims.visit_id = source.visit_id 
+		AND dims.visitor_id = source.visitor_id 
+		AND dims.visit_start_time = source.visit_start_time
+	WHERE dims.dimension_scope = 'Hit'
+
 )
 
 SELECT *
-FROM source
+FROM joined
