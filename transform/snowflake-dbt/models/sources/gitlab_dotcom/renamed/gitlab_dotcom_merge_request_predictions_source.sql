9e92c88ca507f1a850ae35b4c192fdafc92c5e15
Peter Empey <pempey@gitlab.com>
2022-12-09 11:44:13 -0700

    updateing format

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_merge_request_predictions_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_merge_request_predictions_source.sql
@@ -2,21 +2,22 @@ WITH source AS (
 
   SELECT *
   FROM {{ ref('gitlab_dotcom_merge_request_predictions_dedupe_source') }}
-  
 
-), renamed AS (
+),
 
-    SELECT
+renamed AS (
 
-      merge_request_id::NUMBER                      AS merge_request_id,
-      created_at::TIMESTAMP                         AS created_at,
-      updated_at::TIMESTAMP                         AS updated_at,
-      suggested_reviewers::VARIANT                  AS obj_suggested_reviewers,
-      obj_suggested_reviewers['top_n']::VARCHAR     AS suggested_reviewers_top_n,
-      obj_suggested_reviewers['version']::VARCHAR   AS suggested_reviewers_version,
-      obj_suggested_reviewers['reviewers']::VARIANT AS suggested_reviewers_list
+  SELECT
 
-    FROM source
+    merge_request_id::NUMBER AS merge_request_id,
+    created_at::TIMESTAMP AS created_at,
+    updated_at::TIMESTAMP AS updated_at,
+    suggested_reviewers::VARIANT AS obj_suggested_reviewers,
+    obj_suggested_reviewers['top_n']::VARCHAR AS suggested_reviewers_top_n,
+    obj_suggested_reviewers['version']::VARCHAR AS suggested_reviewers_version,
+    obj_suggested_reviewers['reviewers']::VARIANT AS suggested_reviewers_list
+
+  FROM source
 
 )
 
