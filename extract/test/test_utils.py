d700a337d45fd7ff9dd00ae2e77aa95a400297eb
Radovan Bacovic <rbacovic@gitlab.com>
2022-12-11 13:19:19 +0100

    Extract and test utils library.

--- extract/test/test_utils.py
+++ extract/test/test_utils.py
@@ -2,14 +2,53 @@
 The main test routine for utils for Automated Service Ping
 """
 
-import pytest
+import os
+from unittest import mock
 
-from extract.saas_usage_ping.utils import EngineFactory
+import pytest
+import requests
+import responses
+
+from extract.saas_usage_ping.utils import EngineFactory, Utils
 
 
 @pytest.fixture(autouse=True)
 def create_engine_factory():
+    """
+    Create class object
+    """
     return EngineFactory()
+
+
+@pytest.fixture(autouse=True)
+def mock_settings_env_vars():
+    """
+    Simulate OS env. variables
+    """
+    with mock.patch.dict(
+        os.environ,
+        {"GITLAB_ANALYTICS_PRIVATE_TOKEN": "xxx"},
+    ):
+        yield
+
+
+@pytest.fixture(autouse=True)
+def create_utils(mock_settings_env_vars):
+    """
+    Create class object
+    """
+    _ = mock_settings_env_vars
+
+    return Utils()
+
+
+@pytest.fixture
+def mocked_responses():
+    """
+    Mock routine to create fake response
+    """
+    with responses.RequestsMock() as rsps:
+        yield rsps
 
 
 def test_engine_factory(create_engine_factory):
@@ -51,7 +90,7 @@ def test_engine_factory_connected(create_engine_factory):
     """
     Test Class properties - connected
     """
-    assert create_engine_factory.connected == False
+    assert create_engine_factory.connected is False
 
 
 def test_connect(create_engine_factory):
@@ -61,3 +100,53 @@ def test_connect(create_engine_factory):
 
     with pytest.raises(KeyError):
         create_engine_factory.connect()
+
+
+def test_utils(create_utils):
+    """
+    Test Class creation
+    """
+    assert create_utils is not None
+
+
+def test_headers(create_utils):
+    """
+    Test Class properties - headers
+    """
+    assert create_utils.headers["PRIVATE-TOKEN"] == "xxx"
+
+
+def test_headers_error(create_utils):
+    """
+    Test Class properties - headers
+    """
+    with pytest.raises(KeyError):
+        assert create_utils.headers["WRONG_KEY"] == "xxx"
+
+
+def test_convert_response_to_json(create_utils, mocked_responses):
+
+    """
+    Test function: convert_response_to_json
+    """
+    expected = {"didi": "pro", "test": "1"}
+    mocked_responses.get(
+        "http://some_gitlab_api_url/test",
+        body='{"test": "1", "didi": "pro"}',
+        status=200,
+        content_type="application/json",
+    )
+
+    resp = requests.get("http://some_gitlab_api_url/test")
+
+    actual = create_utils.convert_response_to_json(response=resp)
+
+    assert actual == expected
+
+
+def test_get_response(create_utils):
+    """
+    Force fake url and raise a Connection Error
+    """
+    with pytest.raises(ConnectionError):
+        _ = create_utils.get_response("http://fake_url/test")
