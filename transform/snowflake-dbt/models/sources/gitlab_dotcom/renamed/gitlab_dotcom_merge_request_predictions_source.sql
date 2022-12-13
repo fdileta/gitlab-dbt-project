32248a9fd5d38daeae369ddde4f08a1dfa504bc9
Peter Empey <pempey@gitlab.com>
2022-12-13 20:04:40 +0000

    correct JSON parsing

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_merge_request_predictions_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_merge_request_predictions_source.sql
@@ -12,7 +12,7 @@ renamed AS (
     merge_request_id::NUMBER AS merge_request_id,
     created_at::TIMESTAMP AS created_at,
     updated_at::TIMESTAMP AS updated_at,
-    suggested_reviewers::VARIANT AS obj_suggested_reviewers,
+    TRY_PARSE_JSON(suggested_reviewers)::VARIANT AS obj_suggested_reviewers,
     obj_suggested_reviewers['top_n']::VARCHAR AS suggested_reviewers_top_n,
     obj_suggested_reviewers['version']::VARCHAR AS suggested_reviewers_version,
     obj_suggested_reviewers['reviewers']::VARIANT AS suggested_reviewers_list
