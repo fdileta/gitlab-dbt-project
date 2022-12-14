823f1bb517cc2502e1c2aaf8a3868cacba7208b2
Peter Empey <pempey@gitlab.com>
2022-12-14 10:26:05 -0700

    force formating

--- transform/snowflake-dbt/models/sources/sheetload/sheetload_leading_organization_entry_dates_source.sql
+++ transform/snowflake-dbt/models/sources/sheetload/sheetload_leading_organization_entry_dates_source.sql
@@ -11,7 +11,8 @@ renamed AS (
     contributor_organization::VARCHAR AS contributor_organization,
     individual_contributor::BOOLEAN AS is_individual_contributor,
     date_of_entry_in_program::DATE AS entry_in_program_date,
-    date_of_exit_of_program::DATE AS exit_of_program_date
+    -- date_of_exit_of_program was empty on first load and was read in as a FLOAT
+    date_of_exit_of_program::VARCHAR::DATE AS exit_of_program_date
   FROM source
 
 )
