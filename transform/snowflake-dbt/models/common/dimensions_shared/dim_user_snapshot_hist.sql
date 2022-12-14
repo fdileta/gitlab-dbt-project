73dcf39ebbc506e79c51d82f6d18f78615bcb098
tpoole1 <tpoole@gmail.com>
2022-12-14 15:20:18 -0500

    changed to snapshots

--- transform/snowflake-dbt/models/common/dimensions_shared/dim_user_snapshot_hist.sql
+++ transform/snowflake-dbt/models/common/dimensions_shared/dim_user_snapshot_hist.sql
@@ -8,7 +8,7 @@
     SELECT
      {{ dbt_utils.surrogate_key(['dbt_updated_at', 'dim_user_sk']) }} AS dim_user_snapshot_hist_id,
      *
-    FROM {{ source('snapshot','dim_user_snapshot') }}
+    FROM {{ source('snapshots','dim_user_snapshot') }}
 
 {{ dbt_audit(
     created_by="@tpoole",
