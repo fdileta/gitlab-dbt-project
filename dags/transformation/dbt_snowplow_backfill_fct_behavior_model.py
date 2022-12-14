c9875d0d712ae8d035ebe9bb66d2635a43f543a2
Chris Sharp <csharp@gitlab.com>
2022-12-14 17:00:10 +0000

    cast variable as DATE

--- dags/transformation/dbt_snowplow_backfill_fct_behavior_model.py
+++ dags/transformation/dbt_snowplow_backfill_fct_behavior_model.py
@@ -100,7 +100,7 @@ def generate_dbt_command(vars_dict):
     dbt_generate_command = f""" 
         {dbt_install_deps_nosha_cmd} &&
         export SNOWFLAKE_TRANSFORM_WAREHOUSE="TRANSFORMING_XL" &&
-        dbt run --profiles-dir profile --target {target} --models {snowplow_model_to_full_refresh} --vars '{var_yaml}'; ret=$?;
+        dbt run --profiles-dir profile --target {target} --models {snowplow_model_to_full_refresh} --vars "{var_yaml}"; ret=$?;
         """
 
     return KubernetesPodOperator(
