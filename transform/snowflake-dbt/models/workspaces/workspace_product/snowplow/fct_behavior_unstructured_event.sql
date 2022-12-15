c10766d0de547cfb8e3ba6ab2d3c41d5c34da65b
Chris Sharp <csharp@gitlab.com>
2022-12-15 11:51:41 +0000

    fix var yaml

--- transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_unstructured_event.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_product/snowplow/fct_behavior_unstructured_event.sql
@@ -42,8 +42,8 @@
 
     {% if is_incremental() %}
 
-      AND behavior_at > (SELECT MAX({{ var('incremental_backfill_date'::DATE, 'behavior_at') }}) FROM {{ this }})
-      AND behavior_at <= (SELECT DATEADD(month, 1,  MAX({{ var('incremental_backfill_date'::DATE, 'behavior_at') }}) )  FROM {{ this }})
+      AND behavior_at > (SELECT MAX({{ var('incremental_backfill_date', 'behavior_at') }}) FROM {{ this }})
+      AND behavior_at <= (SELECT DATEADD(month, 1,  MAX({{ var('incremental_backfill_date', 'behavior_at') }}) )  FROM {{ this }})
 
     {% endif %}
 
