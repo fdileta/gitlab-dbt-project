8dc1a7f03c64da65c9306ce8df56441e27f49fe6
tpoole1 <tpoole@gmail.com>
2022-12-15 14:15:47 -0500

    add cte_ref to Hist

--- transform/snowflake-dbt/models/common/dimensions_shared/dim_user_snapshot_hist.sql
+++ transform/snowflake-dbt/models/common/dimensions_shared/dim_user_snapshot_hist.sql
@@ -5,12 +5,17 @@
     })
 }}
 
+WITH dim_user_snapshot AS (
+ 
     SELECT
      {{ dbt_utils.surrogate_key(['dbt_updated_at', 'dim_user_sk']) }} AS dim_user_snapshot_hist_id,
      *
     FROM {{ source('snapshots','dim_user_snapshot') }}
 
+)    
+
 {{ dbt_audit(
+    cte_ref = "dim_user",
     created_by="@tpoole",
     updated_by="@tpoole",
     created_date="2022-12-14",
