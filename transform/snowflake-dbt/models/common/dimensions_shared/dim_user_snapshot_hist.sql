81f295fce429da3db54991d0b0b10f6250cd8614
tpoole1 <tpoole@gmail.com>
2022-12-19 15:15:29 -0500

    Run Lintr

--- transform/snowflake-dbt/models/common/dimensions_shared/dim_user_snapshot_hist.sql
+++ transform/snowflake-dbt/models/common/dimensions_shared/dim_user_snapshot_hist.sql
@@ -6,18 +6,18 @@
 }}
 
 WITH dim_user_snapshot AS (
- 
-    SELECT
-     {{ dbt_utils.surrogate_key(['dbt_updated_at', 'dim_user_sk']) }} AS dim_user_snapshot_hist_id,
-     *
-    FROM {{ source('snapshots','dim_user_snapshot') }}
 
-)    
+  SELECT
+    {{ dbt_utils.surrogate_key(['dbt_updated_at', 'dim_user_sk']) }} AS dim_user_snapshot_hist_id,
+    *
+  FROM {{ source('snapshots','dim_user_snapshot') }}
+
+)
 
 {{ dbt_audit(
-    cte_ref = "dim_user",
+    cte_ref = "dim_user_snapshot",
     created_by="@tpoole",
     updated_by="@tpoole",
-    created_date="2022-12-14",
-    updated_date="2022-12-14"
+    created_date="2022-12-19",
+    updated_date="2022-12-19"
 ) }}
