37217d3ab64f353bb3d2d887c514eb2ba6dffcc5
Peter Empey <pempey@gitlab.com>
2022-12-19 07:18:33 -0700

    adding filter to snapshot

--- transform/snowflake-dbt/snapshots/workday/workday_employee_mapping_snapshots.sql
+++ transform/snowflake-dbt/snapshots/workday/workday_employee_mapping_snapshots.sql
@@ -10,5 +10,10 @@
     
     SELECT * 
     FROM {{ source('workday','employee_mapping') }}
+    /* 
+    greenhouse_candidate_id can erroneously and temporally contain text values,
+    such as P-130030639002 causing the snapshot to fail on merging due to data type conflicts
+    */
+    WHERE NOT regexp_like(greenhouse_candidate_id, '[A-Za-z].*')
     
 {% endsnapshot %}
