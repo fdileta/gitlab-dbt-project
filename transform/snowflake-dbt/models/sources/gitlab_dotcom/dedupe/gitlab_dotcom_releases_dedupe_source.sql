10ba5ec234bd8e763e2c9ca1a6412a0e9f942b9d
Miles Russell <mdrussell@gitlab.com>
2022-12-19 16:22:20 -0600

    changing int to number

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_releases_dedupe_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/dedupe/gitlab_dotcom_releases_dedupe_source.sql
@@ -8,10 +8,10 @@ SELECT
   id::NUMBER AS id,
   tag::VARCHAR AS tag,
   description::VARCHAR AS description,
-  project_id::INT AS project_id,
+  project_id::NUMBER AS project_id,
   created_at::TIMESTAMP AS created_at,
   updated_at::TIMESTAMP AS updated_at,
-  author_id::INT AS author_id,
+  author_id::NUMBER AS author_id,
   sha::VARCHAR AS sha,
   _uploaded_at::FLOAT AS _uploaded_at
 FROM {{ source('gitlab_dotcom', 'releases') }}
