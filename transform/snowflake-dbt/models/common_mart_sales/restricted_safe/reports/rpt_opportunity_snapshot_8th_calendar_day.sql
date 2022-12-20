3bc788aca2bb62aa180db016394e764f24cb7089
Lis Vinueza <lvinueza@gitlab.com>
2022-12-21 09:58:52 +1100

    Add 8th calendar day snapshot

---
+++ transform/snowflake-dbt/models/common_mart_sales/restricted_safe/reports/rpt_opportunity_snapshot_8th_calendar_day.sql
@@ -0,0 +1,24 @@
+WITH snapshot_dates AS (
+    --Use the 8th calendar day to snapshot ATR
+    SELECT DISTINCT
+      first_day_of_month,
+      snapshot_date_fpa
+    FROM {{ ref('dim_date') }}
+    ORDER BY 1 DESC
+
+), mart_crm_opportunity_daily_snapshot AS (
+
+    SELECT *
+    FROM {{ ref('mart_crm_opportunity_daily_snapshot') }}
+
+), final AS (
+
+    SELECT *
+    FROM mart_crm_opportunity_daily_snapshot
+    INNER JOIN snapshot_dates
+      ON mart_crm_opportunity_daily_snapshot.snapshot_date = snapshot_dates.snapshot_date_fpa
+
+)
+
+SELECT *
+FROM final
