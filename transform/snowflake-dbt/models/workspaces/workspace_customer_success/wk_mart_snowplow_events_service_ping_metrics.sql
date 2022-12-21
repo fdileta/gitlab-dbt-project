25a9d2c06fff60e7334f32ca8a29cdc21927ee96
Miles Russell <mdrussell@gitlab.com>
2022-12-21 17:00:55 -0600

    adding additional tables and documentation

---
+++ transform/snowflake-dbt/models/workspaces/workspace_customer_success/wk_mart_snowplow_events_service_ping_metrics.sql
@@ -0,0 +1,43 @@
+{{
+  config(
+    materialized='view',
+    tags=["mnpi_exception"]
+  )
+}}
+
+WITH service_ping_events AS (
+  SELECT
+    *
+  FROM {{ ref('wk_fct_snowplow_events_service_ping') }}
+),
+
+metric_bridge AS (
+  SELECT
+    *
+  FROM {{ ref('bdg_metrics_redis_events') }}
+),
+
+joined AS (
+  SELECT
+    service_ping_events.event_id,
+    service_ping_events.derived_tstamp,
+    service_ping_events.gsc_pseudonymized_user_id,
+    service_ping_events.gsc_namespace_id,
+    service_ping_events.gsc_project_id,
+    service_ping_events.gsc_plan,
+    service_ping_events.ultimate_parent_namespace_id,
+    metric_bridge.metrics_path,
+    metric_bridge.aggregate_operator,
+    metric_bridge.aggregate_attribute,
+    metric_bridge.metrics_status
+  FROM service_ping_events
+  INNER JOIN metric_bridge ON service_ping_events.redis_event_name = metric_bridge.redis_event_name
+)
+
+{{ dbt_audit(
+    cte_ref="joined",
+    created_by="@mdrussell",
+    updated_by="@mdrussell",
+    created_date="2022-12-21",
+    updated_date="2022-12-21"
+) }}
---
+++ transform/snowflake-dbt/models/workspaces/workspace_customer_success/wk_rpt_user_based_metric_counts_namespace_monthly.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    materialized='table',
+    tags=["mnpi_exception"]
+  )
+}}
+
+WITH events AS (
+  SELECT
+    *
+  FROM {{ ref('wk_mart_snowplow_events_service_ping_metrics') }}
+  -- only include non-aggregated metrics or aggregated metrics of 'OR' aggregate operator.
+  -- more details here: https://gitlab.com/gitlab-org/gitlab/-/issues/376244#note_1167575425
+  WHERE aggregate_operator = 'OR'
+    OR aggregate_operator IS NULL
+)
+
+final AS (
+  SELECT
+    DATE_TRUNC('month', derived_tstamp) AS date_month,
+    ultimate_parent_namespace_id,
+    metrics_path,
+    COUNT(DISTINCT gsc_pseudonymized_user_id) AS distinct_users
+  FROM events
+  {{ dbt_utils.group_by(n = 3) }}
+)
