17453410ddfd87b83153addea63581450ae2b6c6
Lis Vinueza <lvinueza@gitlab.com>
2022-12-22 06:11:51 +0000

    Add missing fields

--- transform/snowflake-dbt/macros/marts/product/macro_mart_ping_instance_metric.sql
+++ transform/snowflake-dbt/macros/marts/product/macro_mart_ping_instance_metric.sql
@@ -52,6 +52,7 @@
       dim_subscription.subscription_end_month                                     AS subscription_end_month,
       dim_subscription.dim_subscription_id_original                               AS dim_subscription_id_original,
       dim_billing_account.dim_billing_account_id                                  AS dim_billing_account_id,
+      dim_crm_accounts.crm_account_name                                           AS crm_account_name,
       dim_crm_accounts.parent_crm_account_demographics_sales_segment              AS parent_crm_account_sales_segment,
       dim_crm_accounts.parent_crm_account_demographics_territory                  AS parent_crm_account_sales_territory,
       dim_crm_accounts.parent_crm_account_industry                                AS parent_crm_account_industry,
