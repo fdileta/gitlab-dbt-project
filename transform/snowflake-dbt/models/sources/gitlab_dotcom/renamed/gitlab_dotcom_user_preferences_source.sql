acf72cb4ff99eeb47fa26c7b63880f0e9a64a890
Peter Empey <pempey@gitlab.com>
2022-12-23 09:01:50 -0700

    add web ide to view

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_user_preferences_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_user_preferences_source.sql
@@ -29,6 +29,7 @@ WITH source AS (
       tab_width::VARCHAR                  AS tab_width,
       experience_level::NUMBER            AS experience_level,
       view_diffs_file_by_file::BOOLEAN    AS view_diffs_file_by_file
+      use_legacy_web_ide::BOOLEAN         AS does_use_legacy_web_ide
 
     FROM source
 
