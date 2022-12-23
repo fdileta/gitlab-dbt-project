ae4bead94a2d814c43af0bcfa45371c1de13dc51
Justin Wong <juwong@gitlab.com>
2022-12-23 11:48:30 -0500

    linting completed

--- extract/clari/src/clari.py
+++ extract/clari/src/clari.py
@@ -31,12 +31,12 @@ from gitlabdata.orchestration_utils import (
 )
 
 config_dict = os.environ.copy()
-HEADERS = {'apikey': config_dict.get("CLARI_API_KEY")}
-BASE_URL = 'https://api.clari.com/v4'
+HEADERS = {"apikey": config_dict.get("CLARI_API_KEY")}
+BASE_URL = "https://api.clari.com/v4"
 
 
 def _calc_fiscal_quarter(date_time: datetime) -> str:
-    """ Based on datetime object, return it's Gitlab fiscal quarter """
+    """Based on datetime object, return it's Gitlab fiscal quarter"""
     fiscal_year = date_time.year + 1
     if date_time.month in [2, 3, 4]:
         fiscal_quarter = 1
@@ -48,7 +48,7 @@ def _calc_fiscal_quarter(date_time: datetime) -> str:
         fiscal_quarter = 4
 
     # Format the fiscal year and quarter as a string
-    fiscal_year_quarter = f'{fiscal_year}_Q{fiscal_quarter}'
+    fiscal_year_quarter = f"{fiscal_year}_Q{fiscal_quarter}"
     return fiscal_year_quarter
 
 
@@ -61,8 +61,8 @@ def _get_previous_fiscal_quarter(date_time: datetime) -> str:
     current_year_int = int(current_fiscal_quarter[:4])
 
     if current_quarter_int == 1:
-        return f'{current_year_int-1}_Q4'
-    return f'{current_year_int}_Q{current_quarter_int - 1}'
+        return f"{current_year_int-1}_Q4"
+    return f"{current_year_int}_Q{current_quarter_int - 1}"
 
 
 def get_fiscal_quarter() -> str:
@@ -73,15 +73,15 @@ def get_fiscal_quarter() -> str:
     For daily DAG runs, return the current fiscal quarter
     and for quarterly runs, return the previous fiscal quarter
     """
-    execution_date = date_parser.parse(config_dict['execution_date'])
-    task_schedule = config_dict['task_schedule']
+    execution_date = date_parser.parse(config_dict["execution_date"])
+    task_schedule = config_dict["task_schedule"]
 
     print(
-        f'Calculating quarter based on the following task_schedule \
-        and execution_date: {task_schedule} | {execution_date}'
+        f"Calculating quarter based on the following task_schedule \
+        and execution_date: {task_schedule} | {execution_date}"
     )
 
-    if task_schedule == 'daily':
+    if task_schedule == "daily":
         return _calc_fiscal_quarter(execution_date)
 
     # else quarterly task schedule
@@ -96,11 +96,11 @@ def make_request(
     timeout: int = 120,
     data: Optional[Dict[Any, Any]] = None,
     current_retry_count: int = 0,
-    max_retry_count: int = 5
+    max_retry_count: int = 5,
 ) -> requests.models.Response:
-    """ Generic function that handles making GET and POST requests """
+    """Generic function that handles making GET and POST requests"""
     if current_retry_count >= max_retry_count:
-        raise Exception(f'Too many retries when calling the {url}')
+        raise Exception(f"Too many retries when calling the {url}")
     try:
         if request_type == "GET":
             response = requests.get(
@@ -122,10 +122,16 @@ def make_request(
             current_retry_count += 1
             # Make the request again
             return make_request(
-                request_type, url, headers, params, timeout, data,
-                current_retry_count, max_retry_count
+                request_type,
+                url,
+                headers,
+                params,
+                timeout,
+                data,
+                current_retry_count,
+                max_retry_count,
             )
-        error(f'request exception for url {url}, see below')
+        error(f"request exception for url {url}, see below")
         raise
 
 
@@ -133,25 +139,23 @@ def start_export_report(fiscal_quarter: str) -> str:
     """
     Make POST request to start report export for a specific fiscal_quarter
     """
-    forecast_id = 'net_arr'
-    forecast_url = f'{BASE_URL}/export/forecast/{forecast_id}'
+    forecast_id = "net_arr"
+    forecast_url = f"{BASE_URL}/export/forecast/{forecast_id}"
 
-    params = {'timePeriod': fiscal_quarter, 'includeHistorical': True}
-    response = make_request('POST', forecast_url, HEADERS, params)
-    return response.json()['jobId']
+    params = {"timePeriod": fiscal_quarter, "includeHistorical": True}
+    response = make_request("POST", forecast_url, HEADERS, params)
+    return response.json()["jobId"]
 
 
 def get_job_status(job_id: str) -> str:
     """Returns the status of the job with the specified ID."""
-    job_status_url = f'{BASE_URL}/export/jobs/{job_id}'
-    response = make_request('GET', job_status_url, HEADERS)
-    return response.json()['job']["status"]
+    job_status_url = f"{BASE_URL}/export/jobs/{job_id}"
+    response = make_request("GET", job_status_url, HEADERS)
+    return response.json()["job"]["status"]
 
 
 def poll_job_status(
-    job_id: str,
-    wait_interval_seconds: int = 30,
-    max_poll_attempts: int = 5
+    job_id: str, wait_interval_seconds: int = 30, max_poll_attempts: int = 5
 ) -> bool:
     """
     Polls the API for the status of the job with the specified ID,
@@ -165,30 +169,28 @@ def poll_job_status(
         poll_attempts += 1
         print(f"Current status: {status}")
         if status == "DONE":
-            print(
-                f'job_id {job_id} successfully completed, it is ready for export.'
-            )
+            print(f"job_id {job_id} successfully completed, \
+                it is ready for export.")
             return True
 
-        if status in ['ABORTED', 'FAILED', 'CANCELLED']:
-            raise Exception(
-                f'job_id {job_id} failed to complete with {status} status'
-            )
+        if status in ["ABORTED", "FAILED", "CANCELLED"]:
+            raise Exception(f"job_id {job_id} failed to complete \
+                with {status} status")
 
-        if poll_attempts >= max_poll_attempts: # (SCHEDULED, STARTED) status
+        if poll_attempts >= max_poll_attempts:  # (SCHEDULED, STARTED) status
             raise TimeoutError(
-                f'Poll attempts to the job status API for \
+                f"Poll attempts to the job status API for \
                 job_id {job_id} have exceeded \
-                maximum poll attempts, aborting.'
+                maximum poll attempts, aborting."
             )
         time.sleep(wait_interval_seconds)
 
 
 def get_report_results(job_id: str) -> Dict[Any, Any]:
-    """ Get the report results as a json/dict object """
-    results_url = f'{BASE_URL}/export/jobs/{job_id}/results'
-    response = make_request('GET', results_url, HEADERS)
-    info('Successfully obtained report data')
+    """Get the report results as a json/dict object"""
+    results_url = f"{BASE_URL}/export/jobs/{job_id}/results"
+    response = make_request("GET", results_url, HEADERS)
+    info("Successfully obtained report data")
     return response.json()
 
 
@@ -202,18 +204,16 @@ def results_dict_to_dataframe(
         - uploaded_at
     """
     dataframe = pd.DataFrame(columns=["jsontext", "api_fiscal_quarter_name"])
-    dataframe.loc[0] = (
-        [
-            json.dumps(results_dict),
-            fiscal_quarter.replace('_', '-'),  # match dim table formatting
-        ]
-    )
+    dataframe.loc[0] = [
+        json.dumps(results_dict),
+        fiscal_quarter.replace("_", "-"),  # match dim table formatting
+    ]
     return dataframe
 
 
 def upload_dataframe_to_snowflake(dataframe: pd.DataFrame) -> None:
-    """ Uploads Clari dataframe to Snowflake"""
-    info('Uploading dataframe to Snowflake...')
+    """Uploads Clari dataframe to Snowflake"""
+    info("Uploading dataframe to Snowflake...")
     loader_engine = snowflake_engine_factory(config_dict, "LOADER")
 
     dataframe_uploader(
@@ -223,14 +223,14 @@ def upload_dataframe_to_snowflake(dataframe: pd.DataFrame) -> None:
         schema="clari",
         add_uploaded_at=True,
     )
-    info('Successfully uploaded report data to Snowflake')
+    info("Successfully uploaded report data to Snowflake")
     loader_engine.dispose()
 
 
 def main() -> None:
-    """ Main driver function """
+    """Main driver function"""
     fiscal_quarter = get_fiscal_quarter()
-    info(f'Processing fiscal_quarter: {fiscal_quarter}')
+    info(f"Processing fiscal_quarter: {fiscal_quarter}")
     job_id = start_export_report(fiscal_quarter)
     poll_job_status(job_id)
     results_dict = get_report_results(job_id)
@@ -238,7 +238,7 @@ def main() -> None:
     upload_dataframe_to_snowflake(dataframe)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     basicConfig(stream=sys.stdout, level=20)
     getLogger("snowflake.connector.cursor").disabled = True
     main()
