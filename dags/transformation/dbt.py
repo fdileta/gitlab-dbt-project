329a7a6d2661f3a7f59e39e3f7b4af0cb4a8120f
Paul Armstrong <parmstrong@gitlab.com>
2023-01-04 11:12:43 +0200

    Correct run schedule, evaluation schedule.

--- dags/transformation/dbt.py
+++ dags/transformation/dbt.py
@@ -91,7 +91,7 @@ dag = DAG(
     "dbt",
     description="This DAG is responsible for doing incremental model refresh",
     default_args=default_args,
-    schedule_interval="5 9 * * *",
+    schedule_interval="15 9 * * *",
     start_date=datetime(2019, 1, 1, 0, 0, 0),
 )
 dag.doc_md = __doc__
@@ -118,7 +118,7 @@ def dbt_evaluate_run_date(timestamp: datetime, exclude_schedule: str) -> bool:
 
 dbt_evaluate_run_date_task = ShortCircuitOperator(
     task_id="evaluate_dbt_run_date",
-    python_callable=lambda: dbt_evaluate_run_date(datetime.now(), "9 * * WED#1"),
+    python_callable=lambda: dbt_evaluate_run_date(datetime.now(), "15 9 * * WED#1"),
     dag=dag,
 )
 
