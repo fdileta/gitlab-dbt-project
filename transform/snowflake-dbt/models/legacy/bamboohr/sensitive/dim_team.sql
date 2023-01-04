f8d214a0eab4156743e96d5125030e1b743d6df0
Lis Vinueza <lvinueza@gitlab.com>
2023-01-05 10:25:04 +1100

    Add final

--- transform/snowflake-dbt/models/legacy/bamboohr/sensitive/dim_team.sql
+++ transform/snowflake-dbt/models/legacy/bamboohr/sensitive/dim_team.sql
@@ -8,14 +8,18 @@ WITH supervisory_orgs AS (
 recursive_hierarchy AS (
     SELECT
         root.team_id, 
+        root.team_hierarchy_level,
+        root.team_members_count,
+        root.team_manager_inherited,
+        root.team_inactivated,
         root.team_name, 
         root.team_manager_name, 
-        root.team_hierarchy_level, 
-        root.cost_center,
+        root.team_manager_name_id,
         root.team_superior_team_id,
+        root.team_inactivated_date,
         TO_ARRAY(root.valid_to)                   AS valid_to_list,
         TO_ARRAY(root.valid_from)                 AS valid_from_list,
-        TO_ARRAY(root.team_superior_team_id)      AS upstream_organizations
+        TO_ARRAY(root.team_id)                    AS upstream_organizations
     FROM supervisory_orgs AS root
     WHERE team_superior_team_id IS NULL
 
@@ -23,11 +27,15 @@ recursive_hierarchy AS (
 
     SELECT
         iter.team_id, 
+        iter.team_hierarchy_level,
+        iter.team_members_count,
+        iter.team_manager_inherited,
+        iter.team_inactivated,
         iter.team_name, 
         iter.team_manager_name, 
-        iter.team_hierarchy_level, 
-        iter.cost_center,
+        iter.team_manager_name_id,
         iter.team_superior_team_id,
+        iter.team_inactivated_date,
         ARRAY_APPEND(anchor.valid_to_list, iter.valid_to)           AS valid_to_list,
         ARRAY_APPEND(anchor.valid_from_list, iter.valid_from)       AS valid_from_list,
         ARRAY_APPEND(anchor.upstream_organizations, iter.team_id)   AS upstream_organizations
@@ -36,4 +44,20 @@ recursive_hierarchy AS (
         ON iter.team_superior_team_id = anchor.team_id
             AND NOT ARRAY_CONTAINS(iter.team_id::VARIANT, anchor.upstream_organizations)
       
-  ),
+),
+
+cleaned AS (
+
+    SELECT 
+    recursive_hierarchy.*,
+    IFF(recursive_hierarchy.team_id IS NOT NULL,
+          TRUE, FALSE)                                              AS is_currently_valid 
+    FROM recursive_hierarchy
+
+)
+
+SELECT * 
+FROM cleaned
+
+
+
