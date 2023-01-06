3d72ac844a62170d7f7f556269ee038d9ac973e6
Matthew Petersen <mpetersen@gitlab.com>
2023-01-06 16:13:53 +1300

    Add all to CTEs

--- transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_metric_first_last_versions.sql
+++ transform/snowflake-dbt/models/common_mart_product/reports/rpt_ping_metric_first_last_versions.sql
@@ -7,22 +7,12 @@
 {{ simple_cte([
     ('dim_gitlab_releases', 'dim_gitlab_releases'),
     ('dim_ping_metric', 'dim_ping_metric'),
+    ('fct_ping_instance_metric', 'fct_ping_instance_metric'),
     ('dim_ping_instance', 'dim_ping_instance')
     ])
 }}
 
 ,
-
- fct_ping_instance_metric AS (
-
-    SELECT
-    dim_ping_instance_id,
-    dim_instance_id,
-    metrics_path
-    FROM PROD.common.fct_ping_instance_metric
-
-
-),
 final AS (
 
 
@@ -31,16 +21,19 @@ final AS (
       dim_ping_instance.ping_edition,
       dim_ping_instance.version_is_prerelease,
       dim_ping_instance.major_minor_version_id ,
-      major_minor_version,
-      major_version,
-      minor_version,
+      dim_ping_instance.major_minor_version,
+      dim_ping_instance.major_version,
+      dim_ping_instance.minor_version,
       dim_ping_metric.time_frame,
-      dim_installation_id
+      fct_ping_instance_metric.dim_installation_id
     FROM fct_ping_instance_metric
     INNER JOIN dim_ping_metric
       ON fct_ping_instance_metric.metrics_path = dim_ping_metric.metrics_path
     INNER JOIN dim_ping_instance
       ON fct_ping_instance_metric.dim_ping_instance_id = dim_ping_instance.dim_ping_instance_id
+    INNER JOIN dim_gitlab_releases --limit to valid versions
+        ON dim_ping_instance.major_minor_version = dim_gitlab_releases.major_minor_version
+
     WHERE 
       -- Removing SaaS
       fct_ping_instance_metric.dim_instance_id != 'ea8bf810-1d6f-4a6a-b4fd-93e8cbd8b57f'
@@ -98,8 +91,6 @@ final AS (
       -- Get count of installations per each metric/edition
       COUNT(DISTINCT dim_installation_id) OVER (PARTITION BY metrics_path, ping_edition, version_is_prerelease)                                  AS dim_installation_count
     FROM final
-      INNER JOIN dim_gitlab_releases --limit to valid versions
-          ON final.major_minor_version = dim_gitlab_releases.major_minor_version
 
 
 )
