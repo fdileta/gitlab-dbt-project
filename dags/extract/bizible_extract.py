f996575a4cba6d2d7ba16afe1cad9711b6c2374f
Paul Armstrong <parmstrong@gitlab.com>
2023-01-10 12:36:01 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- dags/extract/bizible_extract.py
+++ dags/extract/bizible_extract.py
@@ -96,7 +96,6 @@ for table_name in tables:
             "TASK_INSTANCE": "{{ task_instance_key_str }}",
             "task_id": task_identifier,
             "START_TIME": "{{ execution_date }}",
-            "END_TIME": "{{ next_execution_date }}",
         },
         affinity=get_affinity(False),
         tolerations=get_toleration(False),
