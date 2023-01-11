37ca7047fb7f3ea98d07d7bcefe29550be0ec658
Michelle Cooper <mcooper@gitlab.com>
2023-01-11 10:45:23 -0800

    add  columns to exception list in snapshot vs live

--- transform/snowflake-dbt/models/common_mart_sales/restricted_safe/mart_crm_opportunity_snapshot_vs_live.sql
+++ transform/snowflake-dbt/models/common_mart_sales/restricted_safe/mart_crm_opportunity_snapshot_vs_live.sql
@@ -10,6 +10,7 @@
   SELECT 
     mart_crm_opportunity_daily_snapshot.crm_opportunity_snapshot_id,
     mart_crm_opportunity_daily_snapshot.dim_crm_opportunity_id,
+    mart_crm_opportunity_daily_snapshot.snapshot_id,
     mart_crm_opportunity_daily_snapshot.snapshot_date,
     mart_crm_opportunity_daily_snapshot.snapshot_month,
     mart_crm_opportunity_daily_snapshot.snapshot_fiscal_year,
@@ -19,8 +20,8 @@
     mart_crm_opportunity_daily_snapshot.snapshot_day_of_fiscal_year_normalised,
     mart_crm_opportunity_daily_snapshot.created_in_snapshot_quarter_net_arr,
     mart_crm_opportunity_daily_snapshot.created_in_snapshot_quarter_deal_count,
-    {{ dbt_utils.star(from=ref('mart_crm_opportunity_daily_snapshot'), except=["UPDATED_DATE", "UPDATED_BY", "MODEL_CREATED_DATE", "MODEL_CREATED_BY", "DIM_CRM_OPPORUNITY_ID", "CRM_OPPORTUNITY_SNAPSHOT_ID", "SNAPSHOT_DATE", "SNAPSHOT_MONTH","SNAPSHOT_FISCAL_YEAR","SNAPSHOT_FISCAL_QUARTER_NAME","SNAPSHOT_FISCAL_QUARTER_DATE","SNAPSHOT_DAY_OF_FISCAL_QUARTER_NORMALISED","SNAPSHOT_DAY_OF_FISCAL_YEAR_NORMALISED", "CREATED_IN_SNAPSHOT_QUARTER_NET_ARR","CREATED_IN_SNAPSHOT_QUARTER_DEAL_COUNT"],relation_alias="mart_crm_opportunity_daily_snapshot", suffix="_snapshot")}},
-    {{ dbt_utils.star(from=ref('mart_crm_opportunity'), except=["UPDATED_DATE", "UPDATED_BY", "MODEL_CREATED_DATE", "MODEL_CREATED_BY", "DIM_CRM_OPPORTUNITY_ID"],relation_alias="mart_crm_opportunity", suffix="_live")}}
+    {{ dbt_utils.star(from=ref('mart_crm_opportunity_daily_snapshot'), except=["CREATED_BY", "DBT_CREATED_AT", "DBT_UPDATED_AT", "UPDATED_DATE", "UPDATED_BY", "MODEL_CREATED_DATE", "MODEL_CREATED_BY", "MODEL_UPDATED_DATE", "DIM_CRM_OPPORUNITY_ID", "DIM_CRM_OPPORTUNITY_SNAPSHOT_ID", "CRM_OPPORTUNITY_SNAPSHOT_ID", "DIM_CRM_OPPORTUNITY_ID", "SNAPSHOT_ID", "SNAPSHOT_DATE", "SNAPSHOT_MONTH","SNAPSHOT_FISCAL_YEAR","SNAPSHOT_FISCAL_QUARTER_NAME","SNAPSHOT_FISCAL_QUARTER_DATE","SNAPSHOT_DAY_OF_FISCAL_QUARTER_NORMALISED","SNAPSHOT_DAY_OF_FISCAL_YEAR_NORMALISED", "CREATED_IN_SNAPSHOT_QUARTER_NET_ARR","CREATED_IN_SNAPSHOT_QUARTER_DEAL_COUNT"],relation_alias="mart_crm_opportunity_daily_snapshot", suffix="_SNAPSHOT")}},
+    {{ dbt_utils.star(from=ref('mart_crm_opportunity'), except=["CREATED_BY", "DBT_CREATED_AT", "DBT_UPDATED_AT", "UPDATED_DATE", "UPDATED_BY", "MODEL_CREATED_DATE", "MODEL_CREATED_BY", "MODEL_UPDATED_DATE",  "DIM_CRM_OPPORTUNITY_ID"],relation_alias="mart_crm_opportunity", suffix="_LIVE")}}
 
   FROM mart_crm_opportunity_daily_snapshot
   LEFT JOIN mart_crm_opportunity
