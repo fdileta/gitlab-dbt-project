a272606329a446e45f9f1529719f424eaf356693
Lis Vinueza <lvinueza@gitlab.com>
2023-01-11 01:07:02 +0000

    Add logic to find hierarchy changes

--- transform/snowflake-dbt/models/legacy/bamboohr/sensitive/dim_team.sql
+++ transform/snowflake-dbt/models/legacy/bamboohr/sensitive/dim_team.sql
@@ -1,31 +1,36 @@
+
 WITH source AS (
 
     SELECT *
     FROM {{ ref('workday_supervisory_organizations_source') }}
 
-), 
+), team_superior_groups AS (
 
-supervisory_orgs AS (
-
-    SELECT 
-        team_id                                                                                                                               AS team_id, 
-        team_hierarchy_level                                                                                                                  AS team_hierarchy_level,
-        team_members_count                                                                                                                    AS team_members_count,
-        team_manager_inherited                                                                                                                AS team_manager_inherited,
-        team_inactivated                                                                                                                      AS team_inactivated,
-        team_name                                                                                                                             AS team_name,
-        team_manager_name                                                                                                                     AS team_manager_name,
-        team_manager_name_id                                                                                                                  AS team_manager_name_id,
-        team_superior_team_id                                                                                                                 AS team_superior_team_id,
-        team_inactivated_date                                                                                                                 AS team_inactivated_date,
-        MIN(valid_from)                                                                                                                       AS valid_from,
-        MAX(IFNULL(valid_to, CURRENT_TIMESTAMP()))::TIMESTAMP                                                                                 AS valid_to
+    SELECT
+      team_id,
+      team_superior_team_id,
+      IFNULL(team_superior_team_id, '-1')                                          AS no_null_superior,
+      LAG(no_null_superior, 1, '-1')
+          OVER (PARTITION BY team_id ORDER BY valid_from)                          AS lag_superior_id,
+      CONDITIONAL_TRUE_EVENT(no_null_superior != lag_superior_id)
+                             OVER ( PARTITION BY team_id ORDER BY valid_from)      AS superior_id_group,
+      valid_from, 
+      IFNULL(valid_to, CURRENT_DATE())                                             AS valid_to     
     FROM source
-    {{ dbt_utils.group_by(n=10)}}
 
-),
+), team_superior_change AS (
 
-recursive_hierarchy AS (
+    SELECT
+      team_id,
+      team_superior_team_id,
+      superior_id_group,
+      MIN(valid_from)  AS valid_from,
+      MAX(valid_to)    AS valid_to
+    FROM team_superior_groups
+    GROUP BY 1,2,3
+
+), recursive_hierarchy AS (
+
     SELECT
         root.team_id                                                                                                                          AS team_id, 
         root.team_hierarchy_level                                                                                                             AS team_hierarchy_level,
@@ -42,7 +47,7 @@ recursive_hierarchy AS (
         TO_ARRAY(root.valid_to)                                                                                                               AS valid_to_list,
         TO_ARRAY(root.valid_from)                                                                                                             AS valid_from_list,
         TO_ARRAY(root.team_id)                                                                                                                AS upstream_organizations
-    FROM supervisory_orgs AS root
+    FROM team_superior_change AS root
     WHERE team_superior_team_id IS NULL
 
     UNION ALL 
@@ -64,7 +69,7 @@ recursive_hierarchy AS (
         ARRAY_APPEND(anchor.valid_to_list, iter.valid_to)                                                                                     AS valid_to_list,
         ARRAY_APPEND(anchor.upstream_organizations, iter.team_id)                                                                             AS upstream_organizations
     FROM recursive_hierarchy AS anchor
-    INNER JOIN supervisory_orgs AS iter
+    INNER JOIN team_superior_change AS iter
         ON iter.team_superior_team_id = anchor.team_id
             AND NOT ARRAY_CONTAINS(iter.team_id::VARIANT, anchor.upstream_organizations)
             AND (
@@ -75,13 +80,9 @@ recursive_hierarchy AS (
                     ELSE FALSE
                 END) = TRUE
             
-            
-),
-
-cleaned AS (
+), team_hierarchy AS (
 
     SELECT 
-        {{ dbt_utils.surrogate_key(['team_id', 'valid_to', 'valid_from']) }}                                                                  AS dim_team_sk,
         recursive_hierarchy.team_id                                                                                                           AS team_id, 
         recursive_hierarchy.team_hierarchy_level                                                                                              AS team_hierarchy_level,
         recursive_hierarchy.team_members_count                                                                                                AS team_members_count,
@@ -104,17 +105,42 @@ cleaned AS (
         recursive_hierarchy.upstream_organizations                                                                                            AS hierarchy_levels_array,
         recursive_hierarchy.valid_from_list                                                                                                   AS valid_from_list, 
         recursive_hierarchy.valid_to_list                                                                                                     AS valid_to_list,
-        MIN(from_list.value::TIMESTAMP)                                                                                                       AS hierarchy_valid_from,
-        MAX(to_list.value::TIMESTAMP)                                                                                                         AS hierarchy_valid_to
+        MAX(from_list.value::TIMESTAMP)                                                                                                       AS hierarchy_valid_from,
+        MIN(to_list.value::TIMESTAMP)                                                                                                         AS hierarchy_valid_to
     FROM recursive_hierarchy
     INNER JOIN LATERAL FLATTEN(INPUT =>valid_from_list) from_list
     INNER JOIN LATERAL FLATTEN(INPUT =>valid_to_list) to_list 
     {{ dbt_utils.group_by(n=23)}}
     HAVING hierarchy_valid_to > hierarchy_valid_from
     
- 
-    
+), final AS (
+
+    SELECT 
+        {{ dbt_utils.surrogate_key(['team_id', 'hierarchy_valid_from']) }} AS dim_team_sk,
+        team_id, 
+        team_hierarchy_level,
+        team_members_count,
+        team_manager_inherited,
+        team_inactivated,
+        team_name, 
+        team_manager_name, 
+        team_manager_name_id,
+        team_superior_team_id,
+        team_inactivated_date,
+        hierarchy_level_1,
+        hierarchy_level_2,
+        hierarchy_level_3,
+        hierarchy_level_4,
+        hierarchy_level_5,
+        hierarchy_level_6,
+        hierarchy_level_7,
+        hierarchy_level_8,
+        hierarchy_level_9,
+        hierarchy_levels_array,
+        hierarchy_valid_from,
+        hierarchy_valid_to
+    FROM team_hierarchy
 )
 
 SELECT * 
-FROM cleaned
+FROM final
