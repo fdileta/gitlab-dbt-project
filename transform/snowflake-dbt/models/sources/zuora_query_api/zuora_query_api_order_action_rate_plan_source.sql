e905576af24f40fc7a20fab86b90ae85847d6f14
Chris Sharp <csharp@gitlab.com>
2023-01-12 14:42:32 +0000

    add order action rate plan source

---
+++ transform/snowflake-dbt/models/sources/zuora_query_api/zuora_query_api_order_action_rate_plan_source.sql
@@ -0,0 +1,23 @@
+WITH source AS (
+
+    SELECT *
+    FROM {{ source('zuora_query_api', 'order_action_rate_plan') }}
+
+), renamed AS (
+
+    SELECT
+      "Id"::TEXT                                                           AS order_action_rate_plan_id,
+      "OrderActionId"::TEXT                                                AS order_action_id,
+      "RatePlanId"::TEXT                                                   AS rate_plan_id,
+      "DELETED"::TEXT                                                      AS deleted,
+      "CreatedById"::TEXT                                                  AS created_by_id,
+      TO_TIMESTAMP(CONVERT_TIMEZONE('UTC', "CreatedDate"))::TIMESTAMP      AS created_date,
+      "UpdatedById"                                                        AS updated_by_id,
+      TO_TIMESTAMP(CONVERT_TIMEZONE('UTC',"UpdatedDate"))::TIMESTAMP       AS updated_date,
+      TO_TIMESTAMP_NTZ(CAST(_uploaded_at AS INT))::TIMESTAMP               AS uploaded_at
+    FROM source
+
+)
+
+SELECT *
+FROM renamed
