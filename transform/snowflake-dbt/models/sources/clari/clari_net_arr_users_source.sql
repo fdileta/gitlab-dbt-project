af8cfd237d568e40f23929ef03819c43670911d4
Justin Wong <juwong@gitlab.com>
2023-01-12 10:52:02 -0500

    Fix correlated query error

--- transform/snowflake-dbt/models/sources/clari/clari_net_arr_users_source.sql
+++ transform/snowflake-dbt/models/sources/clari/clari_net_arr_users_source.sql
@@ -19,7 +19,7 @@ intermediate AS (
     LATERAL FLATTEN(input => jsontext:data:users) AS d
 
   {% if is_incremental() %}
-    WHERE source.uploaded_at > (SELECT MAX(source.uploaded_at) FROM {{ this }})
+    WHERE source.uploaded_at > (SELECT MAX(t.uploaded_at) FROM {{ this }} as t)
   {% endif %}
 ),
 
