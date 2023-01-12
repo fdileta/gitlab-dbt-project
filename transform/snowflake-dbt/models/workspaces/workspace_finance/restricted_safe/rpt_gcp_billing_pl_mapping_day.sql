808183362f99eef22e0456b45cbd798be7b2033b
Clement Leroux <cleroux@gitlab.com>
2023-01-12 12:14:03 +0100

    add pl model

--- transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/rpt_gcp_billing_pl_mapping_day.sql
+++ transform/snowflake-dbt/models/workspaces/workspace_finance/restricted_safe/rpt_gcp_billing_pl_mapping_day.sql
@@ -4,44 +4,47 @@
 }}
 
 
-WITH services AS (
+WITH service_base AS (
 
-  SELECT * FROM {{ ref('gcp_billing_pl_mapping_day') }}
+  SELECT * FROM {{ ref('rpt_gcp_billing_service_mapping_day') }}
+
 
 ),
 
 allocation as (
 
-    SELECT * FROM {{ ref{'gcp_billing_service_allocation.csv'} }}
+  SELECT * FROM {{ ref('gcp_billing_service_allocation') }}
+  unpivot(allocation for type in (free, internal, paid))
 
 ),
 
+sandbox as (
+
+  SELECT * FROM {{ ref('gcp_billing_sandbox_projects') }}
+
+)
+
+SELECT
+service_base.day,
+service_base.gcp_project_id,
+service_base.gcp_service_description,
+service_base.gcp_sku_description,
+service_base.gitlab_service,
+coalesce(allocation.type, sandbox.classification, 'unknown') as finance_pl,
+service_base.net_cost * coalesce(allocation.allocation, 1) as net_cost
+FROM
+service_base
+LEFT JOIN allocation on allocation.service = service_base.gitlab_service
+LEFT JOIN sandbox on sandbox.project_name = service_base.gcp_project_id
 
 
-hvs AS (
-SELECT
-inte.day,
-inte.project_id AS gcp_project_id,
-inte.service AS gcp_service_description,
-inte.sku_description AS gcp_sku_description,
-inte.sku_subtype AS finance_new_sku_subtype,
-inte.networking_allocation_sku_mapping AS finance_old_sku_type,
-inte.gl_product_category AS gl_label_product_category,
-inte.product_gl_service AS product_gl_service,
-CASE
-    WHEN product_gl_service IS NOT NULL THEN 'COGS OK: gitlab service'
-    WHEN gl_label_product_category IS NOT NULL THEN 'COGS OK: infra label'
-    WHEN gcp_service_description = 'Support' THEN 'COGS OK: Support is internal'
-    WHEN regexp_like(inte.project_id, '.*([0-9])') -- contains a number 
-    AND regexp_like(inte.project_id, '([a-z]){0,15}-[0-9a-z]{8}$') THEN 'COGS OK: sandbox'
-    WHEN lower(gcp_sku_description) like '%commitment%' THEN 'COGS OK: same split as the rest of compute engine'
-    WHEN lower(gcp_service_description) like '%security command center%' THEN 'COGS OK: security internal?' -- or split logic with top customers network transfers?
-END
-AS finance_hvs,
-inte.net_cost AS net_cost
-FROM
-inte)
-SELECT
-*
-FROM
-hvs
+-- CASE
+--     WHEN product_gl_service IS NOT NULL THEN 'COGS OK: gitlab service'
+--     WHEN gl_label_product_category IS NOT NULL THEN 'COGS OK: infra label'
+--     WHEN gcp_service_description = 'Support' THEN 'COGS OK: Support is internal'
+--     WHEN regexp_like(inte.project_id, '.*([0-9])') -- contains a number 
+--     AND regexp_like(inte.project_id, '([a-z]){0,15}-[0-9a-z]{8}$') THEN 'COGS OK: sandbox'
+--     WHEN lower(gcp_sku_description) like '%commitment%' THEN 'COGS OK: same split as the rest of compute engine'
+--     WHEN lower(gcp_service_description) like '%security command center%' THEN 'COGS OK: security internal?' -- or split logic with top customers network transfers?
+-- END
+-- AS finance_hvs,
