6186a60efaa93b0ab8460544fb7730c635de1789
Snehansh Chaturvedi <schaturvedi@gitlab.com>
2023-01-16 16:05:53 +0530

    Corrected changes

--- transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_pages_domains_source.sql
+++ transform/snowflake-dbt/models/sources/gitlab_dotcom/renamed/gitlab_dotcom_pages_domains_source.sql
@@ -8,16 +8,16 @@ WITH source AS (
   
     SELECT
     
-      id::NUMBER                             AS pages_domain_id,
-      project_id::NUMBER                     AS project_id,
+      id::NUMBER                              AS pages_domain_id,
+      project_id::NUMBER                      AS project_id,
       verified_at::TIMESTAMP                  AS verified_at,
-      verification_code::VARCHAR              AS verification_code,
       enabled_until::TIMESTAMP                AS enabled_until,
       remove_at::TIMESTAMP                    AS remove_at,
       auto_ssl_enabled::BOOLEAN               AS is_auto_ssl_enabled,
-      certificate_valid_not_before::TIMESTAMP AS certificate_valid_not_before,
-      certificate_valid_not_after::TIMESTAMP  AS certificate_valid_not_after,
-      certificate_source::VARCHAR             AS certificate_source
+      wildcard::BOOLEAN                       AS wildcard,
+      usage::NUMBER                           AS usage,
+      scope::NUMBER                           AS scope,
+      auto_ssl_failed:BOOLEAN                 AS is_auto_ssl_failed
     
     FROM source
       
